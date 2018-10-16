872f343ba8028d48bf9d47f6c7b8a6501f55c401
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2018-10-16 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1446,
-	"created_at": "Sun Oct 14 06:12:14 +0000 2018",
+	"tweets": 1447,
+	"created_at": "Tue Oct 16 06:12:15 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_10.js
+++ data/js/tweets/2018_10.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2018_10 = 
 [
 	{
+		"created_at": "Mon Oct 15 13:23:41 +0000 2018",
+		"id": 1051825944829276200,
+		"id_str": "1051825944829276160",
+		"full_text": "Never give up, and the lows will always turn back to highs 💪💪⠀\n⠀\n#inspiremyinstagram #meetthemoment#seekthepositive #inspiremyinstagram #meetthemoment#seekthepositive https://t.co/iqn4G7Ogdt https://t.co/nhpPT25W2p",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			190
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "inspiremyinstagram",
+					"indices": [
+						65,
+						84
+					]
+				},
+				{
+					"text": "inspiremyinstagram",
+					"indices": [
+						116,
+						135
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/iqn4G7Ogdt",
+					"expanded_url": "https://www.instagram.com/p/Bo9CiDoDusi/",
+					"display_url": "instagram.com/p/Bo9CiDoDusi/",
+					"indices": [
+						167,
+						190
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1051825943499731000,
+					"id_str": "1051825943499730945",
+					"indices": [
+						191,
+						214
+					],
+					"media_url": "http://pbs.twimg.com/media/DpjWH2_XcAEuDKM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DpjWH2_XcAEuDKM.jpg",
+					"url": "https://t.co/nhpPT25W2p",
+					"display_url": "pic.twitter.com/nhpPT25W2p",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1051825944829276160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1051825943499731000,
+					"id_str": "1051825943499730945",
+					"indices": [
+						191,
+						214
+					],
+					"media_url": "http://pbs.twimg.com/media/DpjWH2_XcAEuDKM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DpjWH2_XcAEuDKM.jpg",
+					"url": "https://t.co/nhpPT25W2p",
+					"display_url": "pic.twitter.com/nhpPT25W2p",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1051825944829276160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing Team 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7239,
+			"friends_count": 4559,
+			"listed_count": 41,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6376,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1442,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Never give up, and the lows will always turn back to highs 💪💪⠀\n⠀\n#inspiremyinstagram #meetthemoment#seekthepositive #inspiremyinstagram #meetthemoment#seekthepositive https://t.co/iqn4G7Ogdt https://t.co/nhpPT25W2p"
+	},
+	{
 		"created_at": "Sat Oct 13 12:23:36 +0000 2018",
 		"id": 1051086046581219300,
 		"id_str": "1051086046581219336",
