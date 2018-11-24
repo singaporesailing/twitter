e26f85b01ac46b2745073936e25cf48533516c7d
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2018-11-24 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1545,
-	"created_at": "Fri Nov 23 06:12:15 +0000 2018",
+	"tweets": 1546,
+	"created_at": "Sat Nov 24 06:12:14 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Fri Nov 23 11:23:48 +0000 2018",
+		"id": 1065928899757056000,
+		"id_str": "1065928899757056002",
+		"full_text": "The path to victory is never easy. The harder you work, the more rewarding victory would be.⠀\n⠀\nWe wish our Laser Radial sailors good conditions on their last three days of their training camp in Malta! 💪🇸🇬⠀\n⠀\nAre you working hard towards your go… https://t.co/oQOtBpqzwt https://t.co/sSLAikNfi1",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/oQOtBpqzwt",
+					"expanded_url": "https://www.instagram.com/p/BqhUW8YBHWT/",
+					"display_url": "instagram.com/p/BqhUW8YBHWT/",
+					"indices": [
+						248,
+						271
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1065928898750369800,
+					"id_str": "1065928898750369792",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/Dsrwr5XW0AAOF_f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dsrwr5XW0AAOF_f.jpg",
+					"url": "https://t.co/sSLAikNfi1",
+					"display_url": "pic.twitter.com/sSLAikNfi1",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1065928899757056002/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1065928898750369800,
+					"id_str": "1065928898750369792",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/Dsrwr5XW0AAOF_f.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dsrwr5XW0AAOF_f.jpg",
+					"url": "https://t.co/sSLAikNfi1",
+					"display_url": "pic.twitter.com/sSLAikNfi1",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1065928899757056002/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7252,
+			"friends_count": 6132,
+			"listed_count": 39,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6401,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1506,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The path to victory is never easy. The harder you work, the more rewarding victory would be.⠀\n⠀\nWe wish our Laser Radial sailors good conditions on their last three days of their training camp in Malta! 💪🇸🇬⠀\n⠀\nAre you working hard towards your go… https://t.co/oQOtBpqzwt https://t.co/sSLAikNfi1"
+	},
+	{
 		"created_at": "Fri Nov 23 01:01:11 +0000 2018",
 		"id": 1065772213746483200,
 		"id_str": "1065772213746483200",
