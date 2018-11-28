7365ca8c60c6eaf1db9215bfbc528e5b91b6cdab
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2018-11-28 14:13:57 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1547,
-	"created_at": "Tue Nov 27 06:12:15 +0000 2018",
+	"tweets": 1548,
+	"created_at": "Wed Nov 28 06:13:56 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_11.js
+++ data/js/tweets/2018_11.js
@@ -1,6 +1,317 @@
 Grailbird.data.tweets_2018_11 = 
 [
 	{
+		"created_at": "Tue Nov 27 14:23:53 +0000 2018",
+		"id": 1067423773228482600,
+		"id_str": "1067423773228482560",
+		"full_text": "Winning starts with your mind.⠀\n⠀\n#quote #qotd #quotes #quoteoftheday #inspire #inspiring #motivation #quotestoliveby #quotesdaily #quotesoftheday #instaquote #quotestagram #lifequotes #dailyquotes #wordsofwisdom #lifelessons #thoughtoftheday #positi… https://t.co/LW56FYyycq https://t.co/dgOD4Xl0Ya",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "quote",
+					"indices": [
+						34,
+						40
+					]
+				},
+				{
+					"text": "qotd",
+					"indices": [
+						41,
+						46
+					]
+				},
+				{
+					"text": "quotes",
+					"indices": [
+						47,
+						54
+					]
+				},
+				{
+					"text": "quoteoftheday",
+					"indices": [
+						55,
+						69
+					]
+				},
+				{
+					"text": "inspire",
+					"indices": [
+						70,
+						78
+					]
+				},
+				{
+					"text": "inspiring",
+					"indices": [
+						79,
+						89
+					]
+				},
+				{
+					"text": "motivation",
+					"indices": [
+						90,
+						101
+					]
+				},
+				{
+					"text": "quotestoliveby",
+					"indices": [
+						102,
+						117
+					]
+				},
+				{
+					"text": "quotesdaily",
+					"indices": [
+						118,
+						130
+					]
+				},
+				{
+					"text": "quotesoftheday",
+					"indices": [
+						131,
+						146
+					]
+				},
+				{
+					"text": "instaquote",
+					"indices": [
+						147,
+						158
+					]
+				},
+				{
+					"text": "quotestagram",
+					"indices": [
+						159,
+						172
+					]
+				},
+				{
+					"text": "lifequotes",
+					"indices": [
+						173,
+						184
+					]
+				},
+				{
+					"text": "dailyquotes",
+					"indices": [
+						185,
+						197
+					]
+				},
+				{
+					"text": "wordsofwisdom",
+					"indices": [
+						198,
+						212
+					]
+				},
+				{
+					"text": "lifelessons",
+					"indices": [
+						213,
+						225
+					]
+				},
+				{
+					"text": "thoughtoftheday",
+					"indices": [
+						226,
+						242
+					]
+				},
+				{
+					"text": "positi",
+					"indices": [
+						243,
+						250
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/LW56FYyycq",
+					"expanded_url": "https://www.instagram.com/p/Bqr36QhhVL_/",
+					"display_url": "instagram.com/p/Bqr36QhhVL_/",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1067423772163145700,
+					"id_str": "1067423772163145728",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/DtBAQ-yXgAAh7NC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtBAQ-yXgAAh7NC.jpg",
+					"url": "https://t.co/dgOD4Xl0Ya",
+					"display_url": "pic.twitter.com/dgOD4Xl0Ya",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1067423773228482560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1067423772163145700,
+					"id_str": "1067423772163145728",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/DtBAQ-yXgAAh7NC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtBAQ-yXgAAh7NC.jpg",
+					"url": "https://t.co/dgOD4Xl0Ya",
+					"display_url": "pic.twitter.com/dgOD4Xl0Ya",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1067423773228482560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7281,
+			"friends_count": 6636,
+			"listed_count": 39,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6399,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1508,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Winning starts with your mind.⠀\n⠀\n#quote #qotd #quotes #quoteoftheday #inspire #inspiring #motivation #quotestoliveby #quotesdaily #quotesoftheday #instaquote #quotestagram #lifequotes #dailyquotes #wordsofwisdom #lifelessons #thoughtoftheday #positi… https://t.co/LW56FYyycq https://t.co/dgOD4Xl0Ya"
+	},
+	{
 		"created_at": "Mon Nov 26 12:23:41 +0000 2018",
 		"id": 1067031134226341900,
 		"id_str": "1067031134226341892",
