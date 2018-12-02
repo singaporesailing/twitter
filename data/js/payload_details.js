0708a7ad4e255e493b9b8930c312e728e74bcbd2
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2018-12-02 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1558,
-	"created_at": "Sat Dec 01 06:12:15 +0000 2018",
+	"tweets": 1559,
+	"created_at": "Sun Dec 02 06:12:15 +0000 2018",
 	"lang": "en"
 }
---
+++ data/js/tweets/2018_12.js
@@ -0,0 +1,279 @@
+Grailbird.data.tweets_2018_12 = 
+[
+	{
+		"created_at": "Sat Dec 01 13:23:42 +0000 2018",
+		"id": 1068858176232190000,
+		"id_str": "1068858176232189953",
+		"full_text": "Every day is a new canvas, you decide how beautiful it is.⠀⠀⠀\n⠀⠀⠀\n#inspiremyinstagram #meetthemoment#seekthepositive #quote #qotd #quotes #quoteoftheday #inspire #inspiring #motivation #quotestoliveby #quotesdaily #quotesoftheday #instaquote #quo… https://t.co/PlZ2yWhpn2 https://t.co/7kStl3mCua",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "inspiremyinstagram",
+					"indices": [
+						66,
+						85
+					]
+				},
+				{
+					"text": "quote",
+					"indices": [
+						117,
+						123
+					]
+				},
+				{
+					"text": "qotd",
+					"indices": [
+						124,
+						129
+					]
+				},
+				{
+					"text": "quotes",
+					"indices": [
+						130,
+						137
+					]
+				},
+				{
+					"text": "quoteoftheday",
+					"indices": [
+						138,
+						152
+					]
+				},
+				{
+					"text": "inspire",
+					"indices": [
+						153,
+						161
+					]
+				},
+				{
+					"text": "inspiring",
+					"indices": [
+						162,
+						172
+					]
+				},
+				{
+					"text": "motivation",
+					"indices": [
+						173,
+						184
+					]
+				},
+				{
+					"text": "quotestoliveby",
+					"indices": [
+						185,
+						200
+					]
+				},
+				{
+					"text": "quotesdaily",
+					"indices": [
+						201,
+						213
+					]
+				},
+				{
+					"text": "quotesoftheday",
+					"indices": [
+						214,
+						229
+					]
+				},
+				{
+					"text": "instaquote",
+					"indices": [
+						230,
+						241
+					]
+				},
+				{
+					"text": "quo",
+					"indices": [
+						242,
+						246
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/PlZ2yWhpn2",
+					"expanded_url": "https://www.instagram.com/p/Bq2IUNqBDK-/",
+					"display_url": "instagram.com/p/Bq2IUNqBDK-/",
+					"indices": [
+						248,
+						271
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1068858174814543900,
+					"id_str": "1068858174814543872",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/DtVY2NEX4AA7pdn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtVY2NEX4AA7pdn.jpg",
+					"url": "https://t.co/7kStl3mCua",
+					"display_url": "pic.twitter.com/7kStl3mCua",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1068858176232189953/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1068858174814543900,
+					"id_str": "1068858174814543872",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/DtVY2NEX4AA7pdn.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DtVY2NEX4AA7pdn.jpg",
+					"url": "https://t.co/7kStl3mCua",
+					"display_url": "pic.twitter.com/7kStl3mCua",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1068858176232189953/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7304,
+			"friends_count": 7231,
+			"listed_count": 39,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6400,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1513,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Every day is a new canvas, you decide how beautiful it is.⠀⠀⠀\n⠀⠀⠀\n#inspiremyinstagram #meetthemoment#seekthepositive #quote #qotd #quotes #quoteoftheday #inspire #inspiring #motivation #quotestoliveby #quotesdaily #quotesoftheday #instaquote #quo… https://t.co/PlZ2yWhpn2 https://t.co/7kStl3mCua"
+	}
+]
