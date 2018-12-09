0b8c5ae2594be5b16906c4e1688a5acf2311ea98
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2018-12-09 14:12:16 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1575,
-	"created_at": "Sat Dec 08 06:12:15 +0000 2018",
+	"tweets": 1576,
+	"created_at": "Sun Dec 09 06:12:15 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,324 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Sat Dec 08 15:31:27 +0000 2018",
+		"id": 1071427040031629300,
+		"id_str": "1071427040031629312",
+		"full_text": "Our Singapore Sailing Federation CEO, Han Ee, was speaking at a Mercedes Benz event on how elite athletes never stop improving! 💪\n⠀\n#neverstopimproving #SailforSG #ItTakesAVillage #MakingWavesTogether #raisingchampions https://t.co/Ih3SQpB3vL",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			218
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "neverstopimproving",
+					"indices": [
+						132,
+						151
+					]
+				},
+				{
+					"text": "SailforSG",
+					"indices": [
+						152,
+						162
+					]
+				},
+				{
+					"text": "ItTakesAVillage",
+					"indices": [
+						163,
+						179
+					]
+				},
+				{
+					"text": "MakingWavesTogether",
+					"indices": [
+						180,
+						200
+					]
+				},
+				{
+					"text": "raisingchampions",
+					"indices": [
+						201,
+						218
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1071426934620344300,
+					"id_str": "1071426934620344320",
+					"indices": [
+						219,
+						242
+					],
+					"media_url": "http://pbs.twimg.com/media/Dt55HtDU0AAi9iE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dt55HtDU0AAi9iE.jpg",
+					"url": "https://t.co/Ih3SQpB3vL",
+					"display_url": "pic.twitter.com/Ih3SQpB3vL",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1071427040031629312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1071426934620344300,
+					"id_str": "1071426934620344320",
+					"indices": [
+						219,
+						242
+					],
+					"media_url": "http://pbs.twimg.com/media/Dt55HtDU0AAi9iE.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dt55HtDU0AAi9iE.jpg",
+					"url": "https://t.co/Ih3SQpB3vL",
+					"display_url": "pic.twitter.com/Ih3SQpB3vL",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1071427040031629312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1536,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 900,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1071426934637097000,
+					"id_str": "1071426934637096960",
+					"indices": [
+						219,
+						242
+					],
+					"media_url": "http://pbs.twimg.com/media/Dt55HtHUcAAJ6nA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dt55HtHUcAAJ6nA.jpg",
+					"url": "https://t.co/Ih3SQpB3vL",
+					"display_url": "pic.twitter.com/Ih3SQpB3vL",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1071427040031629312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 463,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 817,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1394,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1071426934603636700,
+					"id_str": "1071426934603636737",
+					"indices": [
+						219,
+						242
+					],
+					"media_url": "http://pbs.twimg.com/media/Dt55Hs_V4AEKzMt.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dt55Hs_V4AEKzMt.jpg",
+					"url": "https://t.co/Ih3SQpB3vL",
+					"display_url": "pic.twitter.com/Ih3SQpB3vL",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1071427040031629312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 439,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 775,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 2048,
+							"h": 1322,
+							"resize": "fit"
+						}
+					}
+				},
+				{
+					"id": 1071426934599385100,
+					"id_str": "1071426934599385091",
+					"indices": [
+						219,
+						242
+					],
+					"media_url": "http://pbs.twimg.com/media/Dt55Hs-VAAMCk1n.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dt55Hs-VAAMCk1n.jpg",
+					"url": "https://t.co/Ih3SQpB3vL",
+					"display_url": "pic.twitter.com/Ih3SQpB3vL",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1071427040031629312/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 510,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 1536,
+							"h": 2048,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 900,
+							"h": 1200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"http://twitter.com/download/iphone\" rel=\"nofollow\">Twitter for iPhone</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7310,
+			"friends_count": 4299,
+			"listed_count": 39,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6419,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1530,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our Singapore Sailing Federation CEO, Han Ee, was speaking at a Mercedes Benz event on how elite athletes never stop improving! 💪\n⠀\n#neverstopimproving #SailforSG #ItTakesAVillage #MakingWavesTogether #raisingchampions https://t.co/Ih3SQpB3vL"
+	},
+	{
 		"created_at": "Fri Dec 07 14:22:45 +0000 2018",
 		"id": 1071047363064553500,
 		"id_str": "1071047363064553472",
