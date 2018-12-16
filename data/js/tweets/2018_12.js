b1a1f02d77dc0b8807c09bdddbaf4fec34774ee1
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2018-12-16 14:12:16 +0800

    Added 1 tweets

--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Sat Dec 15 12:24:02 +0000 2018",
+		"id": 1073916593120387100,
+		"id_str": "1073916593120387072",
+		"full_text": "Day 2/12: 🍰 THE PRIZE:\nToday, two lucky winners stand a chance to win a delicious Jiggyfluff triple cheese half baked cheese cake from Cat &amp; the Fiddle [@catandthefiddlecakes] worth S$45.90! \nCreated by award-winning pâtissier Daniel Tay (founder of … https://t.co/JnGOqjx0Da https://t.co/TpDcsheZ4u",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JnGOqjx0Da",
+					"expanded_url": "https://www.instagram.com/p/BraEBwpAn70/",
+					"display_url": "instagram.com/p/BraEBwpAn70/",
+					"indices": [
+						256,
+						279
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1073916591123939300,
+					"id_str": "1073916591123939329",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/DudRcyRXQAEdFQy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DudRcyRXQAEdFQy.jpg",
+					"url": "https://t.co/TpDcsheZ4u",
+					"display_url": "pic.twitter.com/TpDcsheZ4u",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1073916593120387072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 642,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 642,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 642,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1073916591123939300,
+					"id_str": "1073916591123939329",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/DudRcyRXQAEdFQy.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DudRcyRXQAEdFQy.jpg",
+					"url": "https://t.co/TpDcsheZ4u",
+					"display_url": "pic.twitter.com/TpDcsheZ4u",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1073916593120387072/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 642,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 642,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 642,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7316,
+			"friends_count": 4618,
+			"listed_count": 39,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6418,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1540,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Day 2/12: 🍰 THE PRIZE:\nToday, two lucky winners stand a chance to win a delicious Jiggyfluff triple cheese half baked cheese cake from Cat &amp; the Fiddle [@catandthefiddlecakes] worth S$45.90! \nCreated by award-winning pâtissier Daniel Tay (founder of … https://t.co/JnGOqjx0Da https://t.co/TpDcsheZ4u"
+	},
+	{
 		"created_at": "Sat Dec 15 02:23:35 +0000 2018",
 		"id": 1073765484930035700,
 		"id_str": "1073765484930035712",
