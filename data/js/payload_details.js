9503a743608fbfce53f0ac8a76d4ce3ecc01e56c
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2018-12-22 14:12:15 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1599,
-	"created_at": "Fri Dec 21 06:12:15 +0000 2018",
+	"tweets": 1601,
+	"created_at": "Sat Dec 22 06:12:15 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,400 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Fri Dec 21 11:30:08 +0000 2018",
+		"id": 1076077353355407400,
+		"id_str": "1076077353355407360",
+		"full_text": "Places are being snapped up as you are reading so, register now!\n 👉 https://t.co/NmyEy3I4iF 👈\n\n#SailforSG #KidsActivities #SGWeekend #EastCoastPark #ActiveSG https://t.co/zQexhemFHT",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			157
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SailforSG",
+					"indices": [
+						95,
+						105
+					]
+				},
+				{
+					"text": "KidsActivities",
+					"indices": [
+						106,
+						121
+					]
+				},
+				{
+					"text": "SGWeekend",
+					"indices": [
+						122,
+						132
+					]
+				},
+				{
+					"text": "EastCoastPark",
+					"indices": [
+						133,
+						147
+					]
+				},
+				{
+					"text": "ActiveSG",
+					"indices": [
+						148,
+						157
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NmyEy3I4iF",
+					"expanded_url": "http://bit.ly/oj-2019",
+					"display_url": "bit.ly/oj-2019",
+					"indices": [
+						68,
+						91
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1076077346229272600,
+					"id_str": "1076077346229272576",
+					"indices": [
+						158,
+						181
+					],
+					"media_url": "http://pbs.twimg.com/media/Du7-pSTXQAA_V-L.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Du7-pSTXQAA_V-L.jpg",
+					"url": "https://t.co/zQexhemFHT",
+					"display_url": "pic.twitter.com/zQexhemFHT",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1076077353355407360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1076077346229272600,
+					"id_str": "1076077346229272576",
+					"indices": [
+						158,
+						181
+					],
+					"media_url": "http://pbs.twimg.com/media/Du7-pSTXQAA_V-L.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Du7-pSTXQAA_V-L.jpg",
+					"url": "https://t.co/zQexhemFHT",
+					"display_url": "pic.twitter.com/zQexhemFHT",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1076077353355407360/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 680,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 800,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7359,
+			"friends_count": 5574,
+			"listed_count": 39,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6418,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1553,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Places are being snapped up as you are reading so, register now!\n 👉 https://t.co/NmyEy3I4iF 👈\n\n#SailforSG #KidsActivities #SGWeekend #EastCoastPark #ActiveSG https://t.co/zQexhemFHT"
+	},
+	{
+		"created_at": "Fri Dec 21 11:00:13 +0000 2018",
+		"id": 1076069824197988400,
+		"id_str": "1076069824197988353",
+		"full_text": "2018 is coming to an end, it's time to get the latest TYR Products at the best prices for this Christmas! With deals up to 40% discount, hurry down and get them while stock lasts! https://t.co/gJAzs96tot",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			179
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [],
+			"media": [
+				{
+					"id": 1076069819009679400,
+					"id_str": "1076069819009679360",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/Du73zJOXQAAmwnD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Du73zJOXQAAmwnD.jpg",
+					"url": "https://t.co/gJAzs96tot",
+					"display_url": "pic.twitter.com/gJAzs96tot",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1076069824197988353/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 274,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 458,
+							"h": 1138,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 458,
+							"h": 1138,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1076069819009679400,
+					"id_str": "1076069819009679360",
+					"indices": [
+						180,
+						203
+					],
+					"media_url": "http://pbs.twimg.com/media/Du73zJOXQAAmwnD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Du73zJOXQAAmwnD.jpg",
+					"url": "https://t.co/gJAzs96tot",
+					"display_url": "pic.twitter.com/gJAzs96tot",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1076069824197988353/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 274,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 458,
+							"h": 1138,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 458,
+							"h": 1138,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7359,
+			"friends_count": 5574,
+			"listed_count": 39,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6418,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1553,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "2018 is coming to an end, it's time to get the latest TYR Products at the best prices for this Christmas! With deals up to 40% discount, hurry down and get them while stock lasts! https://t.co/gJAzs96tot"
+	},
+	{
 		"created_at": "Fri Dec 21 02:23:46 +0000 2018",
 		"id": 1075939856059195400,
 		"id_str": "1075939856059195392",
