9842d4a469eb1aba76303df967a47ba2884b8dc6
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2018-12-24 14:12:16 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1602,
-	"created_at": "Sun Dec 23 06:12:15 +0000 2018",
+	"tweets": 1603,
+	"created_at": "Mon Dec 24 06:12:15 +0000 2018",
 	"lang": "en"
 }
--- data/js/tweets/2018_12.js
+++ data/js/tweets/2018_12.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2018_12 = 
 [
 	{
+		"created_at": "Sun Dec 23 12:23:42 +0000 2018",
+		"id": 1076815610791772200,
+		"id_str": "1076815610791772160",
+		"full_text": "DAY 10/12: Tonight's giveaway is sure to be a crowd pleaser and perfect for taking your bae 😍\n.\n. 🎁 THE PRIZE:\nSwisshotel Merchant Court [@swisshotelmerchentcourt] is giving away a buffet dinner at the Ellenborough market Cafe for two worth S$184 net… https://t.co/0WNRT0hN4T https://t.co/htR9QMWVb9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0WNRT0hN4T",
+					"expanded_url": "https://www.instagram.com/p/BruqUeHArev/",
+					"display_url": "instagram.com/p/BruqUeHArev/",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1076815609718018000,
+					"id_str": "1076815609718018048",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/DvGeF4HWkAAuntX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DvGeF4HWkAAuntX.jpg",
+					"url": "https://t.co/htR9QMWVb9",
+					"display_url": "pic.twitter.com/htR9QMWVb9",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1076815610791772160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 647,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 647,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 647,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1076815609718018000,
+					"id_str": "1076815609718018048",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/DvGeF4HWkAAuntX.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DvGeF4HWkAAuntX.jpg",
+					"url": "https://t.co/htR9QMWVb9",
+					"display_url": "pic.twitter.com/htR9QMWVb9",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1076815610791772160/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 647,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 647,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 647,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7372,
+			"friends_count": 6358,
+			"listed_count": 39,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6418,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1555,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "DAY 10/12: Tonight's giveaway is sure to be a crowd pleaser and perfect for taking your bae 😍\n.\n. 🎁 THE PRIZE:\nSwisshotel Merchant Court [@swisshotelmerchentcourt] is giving away a buffet dinner at the Ellenborough market Cafe for two worth S$184 net… https://t.co/0WNRT0hN4T https://t.co/htR9QMWVb9"
+	},
+	{
 		"created_at": "Sun Dec 23 02:23:52 +0000 2018",
 		"id": 1076664656780058600,
 		"id_str": "1076664656780058624",
