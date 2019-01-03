54c2d764be0d07291d8714129851298730c1f95b
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-01-03 14:12:15 +0800

    Added 2 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1614,
-	"created_at": "Wed Jan 02 06:12:15 +0000 2019",
+	"tweets": 1616,
+	"created_at": "Thu Jan 03 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,403 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Wed Jan 02 11:23:36 +0000 2019",
+		"id": 1080424363550298100,
+		"id_str": "1080424363550298112",
+		"full_text": "Our first day of school 🤓\n\n#SailforSG #ItTakesAVillage #MakingWavesTogether #RaisingChampions https://t.co/pfy8HZm7bT https://t.co/wzj4ojiFA9",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			117
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SailforSG",
+					"indices": [
+						27,
+						37
+					]
+				},
+				{
+					"text": "ItTakesAVillage",
+					"indices": [
+						38,
+						54
+					]
+				},
+				{
+					"text": "MakingWavesTogether",
+					"indices": [
+						55,
+						75
+					]
+				},
+				{
+					"text": "RaisingChampions",
+					"indices": [
+						76,
+						93
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/pfy8HZm7bT",
+					"expanded_url": "https://www.instagram.com/p/BsITgzSDUCH/",
+					"display_url": "instagram.com/p/BsITgzSDUCH/",
+					"indices": [
+						94,
+						117
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1080424362514239500,
+					"id_str": "1080424362514239488",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5wO75W0AA-TGp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5wO75W0AA-TGp.jpg",
+					"url": "https://t.co/wzj4ojiFA9",
+					"display_url": "pic.twitter.com/wzj4ojiFA9",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1080424363550298112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1080424362514239500,
+					"id_str": "1080424362514239488",
+					"indices": [
+						118,
+						141
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5wO75W0AA-TGp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5wO75W0AA-TGp.jpg",
+					"url": "https://t.co/wzj4ojiFA9",
+					"display_url": "pic.twitter.com/wzj4ojiFA9",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1080424363550298112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7422,
+			"friends_count": 4444,
+			"listed_count": 39,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6418,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1568,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our first day of school 🤓\n\n#SailforSG #ItTakesAVillage #MakingWavesTogether #RaisingChampions https://t.co/pfy8HZm7bT https://t.co/wzj4ojiFA9"
+	},
+	{
+		"created_at": "Wed Jan 02 11:00:36 +0000 2019",
+		"id": 1080418576417333200,
+		"id_str": "1080418576417333248",
+		"full_text": "Raffles Marina will be organising the 2019 Raffles Marina Optimist Ranking Regatta –  over the weekend of 23 &amp; 24 February.\n\nSign up now to enjoy early bird rates! \n\nSee you there! 👋 https://t.co/utzyDVYONy https://t.co/FlMi7n2U9g",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			210
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/utzyDVYONy",
+					"expanded_url": "https://www.facebook.com/events/1979203138830145/",
+					"display_url": "facebook.com/events/1979203…",
+					"indices": [
+						187,
+						210
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1080418572336287700,
+					"id_str": "1080418572336287745",
+					"indices": [
+						211,
+						234
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5q95zW0AET2jA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5q95zW0AET2jA.jpg",
+					"url": "https://t.co/FlMi7n2U9g",
+					"display_url": "pic.twitter.com/FlMi7n2U9g",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1080418576417333248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 526,
+							"h": 275,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 526,
+							"h": 275,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 526,
+							"h": 275,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1080418572336287700,
+					"id_str": "1080418572336287745",
+					"indices": [
+						211,
+						234
+					],
+					"media_url": "http://pbs.twimg.com/media/Dv5q95zW0AET2jA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/Dv5q95zW0AET2jA.jpg",
+					"url": "https://t.co/FlMi7n2U9g",
+					"display_url": "pic.twitter.com/FlMi7n2U9g",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1080418576417333248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 526,
+							"h": 275,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 526,
+							"h": 275,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 526,
+							"h": 275,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7422,
+			"friends_count": 4444,
+			"listed_count": 39,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6418,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1568,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Raffles Marina will be organising the 2019 Raffles Marina Optimist Ranking Regatta –  over the weekend of 23 &amp; 24 February.\n\nSign up now to enjoy early bird rates! \n\nSee you there! 👋 https://t.co/utzyDVYONy https://t.co/FlMi7n2U9g"
+	},
+	{
 		"created_at": "Tue Jan 01 12:23:46 +0000 2019",
 		"id": 1080077117084311600,
 		"id_str": "1080077117084311553",
