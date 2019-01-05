9b56f9368285b8806f913b5e37bbad9b70760a7c
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-01-05 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1616,
-	"created_at": "Thu Jan 03 06:12:14 +0000 2019",
+	"tweets": 1617,
+	"created_at": "Sat Jan 05 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Sat Jan 05 00:30:13 +0000 2019",
+		"id": 1081347100590387200,
+		"id_str": "1081347100590387200",
+		"full_text": "Here are some of the lessons learned and advice for enthusiastic sailors wanting to take their sailing to the next level! 🙆👏 #SailingIsLife #ILoveSailing #Passion https://t.co/U7j4okgStL https://t.co/AylZOw3LHM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			186
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SailingIsLife",
+					"indices": [
+						125,
+						139
+					]
+				},
+				{
+					"text": "ILoveSailing",
+					"indices": [
+						140,
+						153
+					]
+				},
+				{
+					"text": "Passion",
+					"indices": [
+						154,
+						162
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/U7j4okgStL",
+					"expanded_url": "https://www.quantumsails.com/en/resources-and-expertise/articles/how-to-make-sailing-more-than-just-a-hobby",
+					"display_url": "quantumsails.com/en/resources-a…",
+					"indices": [
+						163,
+						186
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1081347096400330800,
+					"id_str": "1081347096400330752",
+					"indices": [
+						187,
+						210
+					],
+					"media_url": "http://pbs.twimg.com/media/DwG3dHfXgAAODtp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DwG3dHfXgAAODtp.jpg",
+					"url": "https://t.co/AylZOw3LHM",
+					"display_url": "pic.twitter.com/AylZOw3LHM",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1081347100590387200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 700,
+							"h": 481,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 467,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 700,
+							"h": 481,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1081347096400330800,
+					"id_str": "1081347096400330752",
+					"indices": [
+						187,
+						210
+					],
+					"media_url": "http://pbs.twimg.com/media/DwG3dHfXgAAODtp.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DwG3dHfXgAAODtp.jpg",
+					"url": "https://t.co/AylZOw3LHM",
+					"display_url": "pic.twitter.com/AylZOw3LHM",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1081347100590387200/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 700,
+							"h": 481,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 467,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 700,
+							"h": 481,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7422,
+			"friends_count": 4796,
+			"listed_count": 40,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6418,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1569,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Here are some of the lessons learned and advice for enthusiastic sailors wanting to take their sailing to the next level! 🙆👏 #SailingIsLife #ILoveSailing #Passion https://t.co/U7j4okgStL https://t.co/AylZOw3LHM"
+	},
+	{
 		"created_at": "Wed Jan 02 11:23:36 +0000 2019",
 		"id": 1080424363550298100,
 		"id_str": "1080424363550298112",
