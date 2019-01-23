6c99956ca0095aa4ce6586acc3f1ba697a62fa47
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-01-23 14:12:16 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1635,
-	"created_at": "Mon Jan 21 06:12:17 +0000 2019",
+	"tweets": 1636,
+	"created_at": "Wed Jan 23 06:12:15 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_01.js
+++ data/js/tweets/2019_01.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2019_01 = 
 [
 	{
+		"created_at": "Wed Jan 23 00:30:10 +0000 2019",
+		"id": 1087870067793113100,
+		"id_str": "1087870067793113088",
+		"full_text": "#WellnessWednesday We are finally halfway through the week! Press on everyone! Amidst the busy schedules that you may have, never forget to care for yourself. Here are 15 tips on how to love yourself more! #SelfCare #SelfLove #FitnessWorkout https://t.co/vRmMifcucg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			265
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "WellnessWednesday",
+					"indices": [
+						0,
+						18
+					]
+				},
+				{
+					"text": "SelfCare",
+					"indices": [
+						206,
+						215
+					]
+				},
+				{
+					"text": "SelfLove",
+					"indices": [
+						216,
+						225
+					]
+				},
+				{
+					"text": "FitnessWorkout",
+					"indices": [
+						226,
+						241
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vRmMifcucg",
+					"expanded_url": "https://www.health.com/fitness/trainers-self-care-workout-tips",
+					"display_url": "health.com/fitness/traine…",
+					"indices": [
+						242,
+						265
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7561,
+			"friends_count": 5067,
+			"listed_count": 41,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6422,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1594,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 2,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "#WellnessWednesday We are finally halfway through the week! Press on everyone! Amidst the busy schedules that you may have, never forget to care for yourself. Here are 15 tips on how to love yourself more! #SelfCare #SelfLove #FitnessWorkout https://t.co/vRmMifcucg"
+	},
+	{
 		"created_at": "Mon Jan 21 04:23:42 +0000 2019",
 		"id": 1087204063060607000,
 		"id_str": "1087204063060606976",
