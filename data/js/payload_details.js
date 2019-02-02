5eb9e1ee288a4e12286198e4b86b215ac13295b0
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-02-02 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1650,
-	"created_at": "Fri Feb 01 06:12:14 +0000 2019",
+	"tweets": 1651,
+	"created_at": "Sat Feb 02 06:12:14 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_02.js
@@ -0,0 +1,195 @@
+Grailbird.data.tweets_2019_02 = 
+[
+	{
+		"created_at": "Sat Feb 02 00:31:13 +0000 2019",
+		"id": 1091494211143303200,
+		"id_str": "1091494211143303168",
+		"full_text": "Sailing is cited as the \"most inclusive sport\". There are many programmes out there for sailors with disabilities and this article will show you one of them. Remember, they simply have a different ability, not a disability. #SailingIsForEveryone 😊 ⛵ https://t.co/0ubF9C91ht https://t.co/UCpzyT3k3P",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			273
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SailingIsForEveryone",
+					"indices": [
+						224,
+						245
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/0ubF9C91ht",
+					"expanded_url": "https://www.abc.net.au/news/2018-11-30/sailability-helping-disabled-people-find-their-strength-on-water/10550382",
+					"display_url": "abc.net.au/news/2018-11-3…",
+					"indices": [
+						250,
+						273
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1091494206336524300,
+					"id_str": "1091494206336524289",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/DyXEMpsV4AEJ3zR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyXEMpsV4AEJ3zR.jpg",
+					"url": "https://t.co/UCpzyT3k3P",
+					"display_url": "pic.twitter.com/UCpzyT3k3P",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1091494211143303168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 700,
+							"h": 394,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 700,
+							"h": 394,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1091494206336524300,
+					"id_str": "1091494206336524289",
+					"indices": [
+						274,
+						297
+					],
+					"media_url": "http://pbs.twimg.com/media/DyXEMpsV4AEJ3zR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyXEMpsV4AEJ3zR.jpg",
+					"url": "https://t.co/UCpzyT3k3P",
+					"display_url": "pic.twitter.com/UCpzyT3k3P",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1091494211143303168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 700,
+							"h": 394,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 700,
+							"h": 394,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7550,
+			"friends_count": 4968,
+			"listed_count": 41,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6421,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1613,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 9,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Sailing is cited as the \"most inclusive sport\". There are many programmes out there for sailors with disabilities and this article will show you one of them. Remember, they simply have a different ability, not a disability. #SailingIsForEveryone 😊 ⛵ https://t.co/0ubF9C91ht https://t.co/UCpzyT3k3P"
+	}
+]
