e818769d5cb83b450a74c7988e295a3027a32708
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-02-06 14:12:16 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1653,
-	"created_at": "Tue Feb 05 06:12:14 +0000 2019",
+	"tweets": 1654,
+	"created_at": "Wed Feb 06 06:12:15 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Wed Feb 06 00:30:08 +0000 2019",
+		"id": 1092943489166426100,
+		"id_str": "1092943489166426113",
+		"full_text": "Soon, we will need to burn off all the Chinese New Year snacks that we have been munching on. Getting and staying fit can be a challenge. So what's the secret of people who have managed to make exercise a way of life? 🏃 #WellnessWednesday https://t.co/98KuD1yPWw https://t.co/wGTd8BzC39",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			262
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "WellnessWednesday",
+					"indices": [
+						220,
+						238
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/98KuD1yPWw",
+					"expanded_url": "https://www.webmd.com/fitness-exercise/features/10-workout-secrets-expert-exercise-tips#1",
+					"display_url": "webmd.com/fitness-exerci…",
+					"indices": [
+						239,
+						262
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1092943484728889300,
+					"id_str": "1092943484728889344",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/media/DyrqTwbX0AAsijC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyrqTwbX0AAsijC.jpg",
+					"url": "https://t.co/wGTd8BzC39",
+					"display_url": "pic.twitter.com/wGTd8BzC39",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1092943489166426113/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 200,
+							"h": 200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 200,
+							"h": 200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 200,
+							"h": 200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1092943484728889300,
+					"id_str": "1092943484728889344",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/media/DyrqTwbX0AAsijC.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DyrqTwbX0AAsijC.jpg",
+					"url": "https://t.co/wGTd8BzC39",
+					"display_url": "pic.twitter.com/wGTd8BzC39",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1092943489166426113/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 200,
+							"h": 200,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 200,
+							"h": 200,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 200,
+							"h": 200,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7537,
+			"friends_count": 4966,
+			"listed_count": 41,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1618,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Soon, we will need to burn off all the Chinese New Year snacks that we have been munching on. Getting and staying fit can be a challenge. So what's the secret of people who have managed to make exercise a way of life? 🏃 #WellnessWednesday https://t.co/98KuD1yPWw https://t.co/wGTd8BzC39"
+	},
+	{
 		"created_at": "Mon Feb 04 23:00:28 +0000 2019",
 		"id": 1092558537446867000,
 		"id_str": "1092558537446866944",
