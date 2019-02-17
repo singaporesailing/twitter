9a892224470bdcc162d26e70fcb254dc74450b6e
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-02-17 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1664,
-	"created_at": "Sat Feb 16 06:12:14 +0000 2019",
+	"tweets": 1665,
+	"created_at": "Sun Feb 17 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_02.js
+++ data/js/tweets/2019_02.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_02 = 
 [
 	{
+		"created_at": "Sun Feb 17 04:23:40 +0000 2019",
+		"id": 1096988527538176000,
+		"id_str": "1096988527538176000",
+		"full_text": "“The smallest act of kindness is worth more than the grandest intention”⠀\n-Oscar Wilde⠀\n⠀\nToday is Random Acts of Kindness Day! As one person you cannot change the world, but you can change the world of one person, try doing something kind for a rand… https://t.co/X8H6JwKACE https://t.co/8KGn8VH8i3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/X8H6JwKACE",
+					"expanded_url": "https://www.instagram.com/p/Bt97yxhBVu_/",
+					"display_url": "instagram.com/p/Bt97yxhBVu_/",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1096988526334406700,
+					"id_str": "1096988526334406662",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/DzlJQKrXcAYQlxM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzlJQKrXcAYQlxM.jpg",
+					"url": "https://t.co/8KGn8VH8i3",
+					"display_url": "pic.twitter.com/8KGn8VH8i3",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1096988527538176000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1096988526334406700,
+					"id_str": "1096988526334406662",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/DzlJQKrXcAYQlxM.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/DzlJQKrXcAYQlxM.jpg",
+					"url": "https://t.co/8KGn8VH8i3",
+					"display_url": "pic.twitter.com/8KGn8VH8i3",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1096988527538176000/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7611,
+			"friends_count": 5112,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6430,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1631,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "“The smallest act of kindness is worth more than the grandest intention”⠀\n-Oscar Wilde⠀\n⠀\nToday is Random Acts of Kindness Day! As one person you cannot change the world, but you can change the world of one person, try doing something kind for a rand… https://t.co/X8H6JwKACE https://t.co/8KGn8VH8i3"
+	},
+	{
 		"created_at": "Sat Feb 16 00:30:06 +0000 2019",
 		"id": 1096567359622598700,
 		"id_str": "1096567359622598656",
