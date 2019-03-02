cf1a1670c4590feaeecb86b5e2ad10cc62f2075e
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-03-02 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1675,
-	"created_at": "Fri Mar 01 06:12:15 +0000 2019",
+	"tweets": 1676,
+	"created_at": "Sat Mar 02 06:12:14 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_03.js
@@ -0,0 +1,195 @@
+Grailbird.data.tweets_2019_03 = 
+[
+	{
+		"created_at": "Sat Mar 02 00:30:06 +0000 2019",
+		"id": 1101640789921333200,
+		"id_str": "1101640789921333248",
+		"full_text": "\"A smooth sea never made a skilled sailor\"\n- Franklin D. Roosevelt.\n32nd President of the U.S.A.\n\nHeard this quote far too many times? Ever wondered what sailing does to your life? Here are 4 things that the author learnt from sailing. #SailingIsLife https://t.co/ObGd3R3Kng https://t.co/vgpe9Yig2n",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			274
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SailingIsLife",
+					"indices": [
+						236,
+						250
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/ObGd3R3Kng",
+					"expanded_url": "https://www.linkedin.com/pulse/4-things-i-learn-from-sailing-how-does-help-me-my-garriga-nogu%C3%A9s",
+					"display_url": "linkedin.com/pulse/4-things…",
+					"indices": [
+						251,
+						274
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1101640785697665000,
+					"id_str": "1101640785697665024",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/D0nQdTWXcAAypBO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0nQdTWXcAAypBO.jpg",
+					"url": "https://t.co/vgpe9Yig2n",
+					"display_url": "pic.twitter.com/vgpe9Yig2n",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1101640789921333248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 390,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 698,
+							"h": 400,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 698,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1101640785697665000,
+					"id_str": "1101640785697665024",
+					"indices": [
+						275,
+						298
+					],
+					"media_url": "http://pbs.twimg.com/media/D0nQdTWXcAAypBO.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0nQdTWXcAAypBO.jpg",
+					"url": "https://t.co/vgpe9Yig2n",
+					"display_url": "pic.twitter.com/vgpe9Yig2n",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1101640789921333248/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 390,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 698,
+							"h": 400,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 698,
+							"h": 400,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7504,
+			"friends_count": 5051,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6433,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1648,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"A smooth sea never made a skilled sailor\"\n- Franklin D. Roosevelt.\n32nd President of the U.S.A.\n\nHeard this quote far too many times? Ever wondered what sailing does to your life? Here are 4 things that the author learnt from sailing. #SailingIsLife https://t.co/ObGd3R3Kng https://t.co/vgpe9Yig2n"
+	}
+]
