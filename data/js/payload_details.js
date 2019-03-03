b0e9473317b855458dee25998cf76d107ec1d51f
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-03-03 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1676,
-	"created_at": "Sat Mar 02 06:12:14 +0000 2019",
+	"tweets": 1677,
+	"created_at": "Sun Mar 03 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Sat Mar 02 12:23:53 +0000 2019",
+		"id": 1101820418174918700,
+		"id_str": "1101820418174918656",
+		"full_text": "Today is measurements day for the Asian Open Laser Championships 2019 at the National Sailing Centre!\n\nStarting Monday, sailors from 15 different countries will be fighting for the title as Asian Laser Champion 🏆\nNamely, Singapore 🇸🇬, Australia 🇦🇺, T… https://t.co/2ZnAGeHrtn https://t.co/vzvwMTYw7s",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/2ZnAGeHrtn",
+					"expanded_url": "https://www.instagram.com/p/BugWUGrhZ8c/",
+					"display_url": "instagram.com/p/BugWUGrhZ8c/",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1101820416430145500,
+					"id_str": "1101820416430145537",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D0pz1L-XgAEYVkG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0pz1L-XgAEYVkG.jpg",
+					"url": "https://t.co/vzvwMTYw7s",
+					"display_url": "pic.twitter.com/vzvwMTYw7s",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1101820418174918656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1101820416430145500,
+					"id_str": "1101820416430145537",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D0pz1L-XgAEYVkG.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D0pz1L-XgAEYVkG.jpg",
+					"url": "https://t.co/vzvwMTYw7s",
+					"display_url": "pic.twitter.com/vzvwMTYw7s",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1101820418174918656/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7509,
+			"friends_count": 5050,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6433,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1649,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 13,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today is measurements day for the Asian Open Laser Championships 2019 at the National Sailing Centre!\n\nStarting Monday, sailors from 15 different countries will be fighting for the title as Asian Laser Champion 🏆\nNamely, Singapore 🇸🇬, Australia 🇦🇺, T… https://t.co/2ZnAGeHrtn https://t.co/vzvwMTYw7s"
+	},
+	{
 		"created_at": "Sat Mar 02 00:30:06 +0000 2019",
 		"id": 1101640789921333200,
 		"id_str": "1101640789921333248",
