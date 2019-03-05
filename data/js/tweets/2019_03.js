7539b679cc47acf592f8d71565dcf9fb032b53b8
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-03-05 14:12:15 +0800

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Mon Mar 04 12:23:55 +0000 2019",
+		"id": 1102545203817254900,
+		"id_str": "1102545203817254912",
+		"full_text": "\"The difference between a successful person and the others is not the lack of strength, not a lack of knowledge , but a lack in will.\" - Vince Lombardi Jr. ⠀\n⠀\nVincent Thomas Lombardi was an American football player, coach, and executive in the Natio… https://t.co/hqYBAsAYil https://t.co/PBOPjxKY1q",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/hqYBAsAYil",
+					"expanded_url": "https://www.instagram.com/p/BulhCS8BbXq/",
+					"display_url": "instagram.com/p/BulhCS8BbXq/",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1102545200591892500,
+					"id_str": "1102545200591892481",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D00HBLYXcAEBAkF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D00HBLYXcAEBAkF.jpg",
+					"url": "https://t.co/PBOPjxKY1q",
+					"display_url": "pic.twitter.com/PBOPjxKY1q",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1102545203817254912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1102545200591892500,
+					"id_str": "1102545200591892481",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D00HBLYXcAEBAkF.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D00HBLYXcAEBAkF.jpg",
+					"url": "https://t.co/PBOPjxKY1q",
+					"display_url": "pic.twitter.com/PBOPjxKY1q",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1102545203817254912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7519,
+			"friends_count": 5052,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6432,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1651,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"The difference between a successful person and the others is not the lack of strength, not a lack of knowledge , but a lack in will.\" - Vince Lombardi Jr. ⠀\n⠀\nVincent Thomas Lombardi was an American football player, coach, and executive in the Natio… https://t.co/hqYBAsAYil https://t.co/PBOPjxKY1q"
+	},
+	{
 		"created_at": "Mon Mar 04 00:30:07 +0000 2019",
 		"id": 1102365570694025200,
 		"id_str": "1102365570694025219",
