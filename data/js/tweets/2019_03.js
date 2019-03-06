9148ab71702e37dee7ef4af0550ad80273955305
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-03-06 14:12:15 +0800

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 06 00:30:05 +0000 2019",
+		"id": 1103090335427846100,
+		"id_str": "1103090335427846146",
+		"full_text": "Lifestyle habits in adulthood may increase women’s life expectancy by 14 years, men gain 12. Maintaining these five healthy habits during adulthood may add more than a decade to life expectancy. https://t.co/zwMTScg1mI https://t.co/aF2XJ03Rue",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			218
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/zwMTScg1mI",
+					"expanded_url": "https://www.weforum.org/agenda/2018/05/5-healthy-habits-may-increase-life-expectancy-by-decade-or-more",
+					"display_url": "weforum.org/agenda/2018/05…",
+					"indices": [
+						195,
+						218
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1103090330545594400,
+					"id_str": "1103090330545594369",
+					"indices": [
+						219,
+						242
+					],
+					"media_url": "http://pbs.twimg.com/media/D072z6tWoAELg4K.png",
+					"media_url_https": "https://pbs.twimg.com/media/D072z6tWoAELg4K.png",
+					"url": "https://t.co/aF2XJ03Rue",
+					"display_url": "pic.twitter.com/aF2XJ03Rue",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1103090335427846146/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 608,
+							"h": 625,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 608,
+							"h": 625,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 608,
+							"h": 625,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1103090330545594400,
+					"id_str": "1103090330545594369",
+					"indices": [
+						219,
+						242
+					],
+					"media_url": "http://pbs.twimg.com/media/D072z6tWoAELg4K.png",
+					"media_url_https": "https://pbs.twimg.com/media/D072z6tWoAELg4K.png",
+					"url": "https://t.co/aF2XJ03Rue",
+					"display_url": "pic.twitter.com/aF2XJ03Rue",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1103090335427846146/photo/1",
+					"type": "photo",
+					"sizes": {
+						"medium": {
+							"w": 608,
+							"h": 625,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 608,
+							"h": 625,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 608,
+							"h": 625,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7522,
+			"friends_count": 5052,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6432,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1652,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Lifestyle habits in adulthood may increase women’s life expectancy by 14 years, men gain 12. Maintaining these five healthy habits during adulthood may add more than a decade to life expectancy. https://t.co/zwMTScg1mI https://t.co/aF2XJ03Rue"
+	},
+	{
 		"created_at": "Mon Mar 04 12:23:55 +0000 2019",
 		"id": 1102545203817254900,
 		"id_str": "1102545203817254912",
