def50c886f33cf9b381f90a8b7331b0948ee100f
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-03-12 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1685,
-	"created_at": "Sun Mar 10 06:12:14 +0000 2019",
+	"tweets": 1686,
+	"created_at": "Tue Mar 12 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,120 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Tue Mar 12 00:30:03 +0000 2019",
+		"id": 1105264655411359700,
+		"id_str": "1105264655411359744",
+		"full_text": "Ever heard of snacking on exercise? 😏 Well, it is something like doing some push-ups while the kettle boils in the morning. Watch this interesting Ted talk to find out more! #HealthyTipTuesday https://t.co/31iL84VkIC",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			216
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HealthyTipTuesday",
+					"indices": [
+						174,
+						192
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/31iL84VkIC",
+					"expanded_url": "https://ideas.ted.com/havent-made-it-to-the-gym-yet-try-snacking-on-exercise-instead/?utm_campaign=social&utm_medium=referral&utm_source=facebook.com&utm_content=ideas-blog&utm_term=humanities&fbclid=IwAR2Qtdm3tdRHLZjJjzOBgnd_sN7kZY-qmwMYWhqiq6Nl_iIVpPZP43sf9iYhttps://ideas.ted.com/havent-made-it-to-the-gym-yet-try-snacking-on-exercise-instead/?utm_campaign=social&utm_medium=referral&utm_source=facebook.com&utm_content=ideas-blog&utm_term=humanities&fbclid=IwAR2Qtdm3tdRHLZjJjzOBgnd_sN7kZY-qmwMYWhqiq6Nl_iIVpPZP43sf9iY",
+					"display_url": "ideas.ted.com/havent-made-it…",
+					"indices": [
+						193,
+						216
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7514,
+			"friends_count": 5032,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6433,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1661,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Ever heard of snacking on exercise? 😏 Well, it is something like doing some push-ups while the kettle boils in the morning. Watch this interesting Ted talk to find out more! #HealthyTipTuesday https://t.co/31iL84VkIC"
+	},
+	{
 		"created_at": "Sun Mar 10 00:30:03 +0000 2019",
 		"id": 1104539880170901500,
 		"id_str": "1104539880170901504",
