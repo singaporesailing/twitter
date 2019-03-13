ac3747362b41250089919d7572ffe6d138c24eaf
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-03-13 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1686,
-	"created_at": "Tue Mar 12 06:12:14 +0000 2019",
+	"tweets": 1687,
+	"created_at": "Wed Mar 13 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,212 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 13 00:23:44 +0000 2019",
+		"id": 1105625454042583000,
+		"id_str": "1105625454042583040",
+		"full_text": "500 days to go to the Tokyo 2020 Olympics! 🇸🇬⛵️💨🏄‍♀️ #roadtotokyo #tokyo2020 #SailforSG https://t.co/VKMPgAHWp0 https://t.co/V8OstdBm5P",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			111
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "roadtotokyo",
+					"indices": [
+						53,
+						65
+					]
+				},
+				{
+					"text": "tokyo2020",
+					"indices": [
+						66,
+						76
+					]
+				},
+				{
+					"text": "SailforSG",
+					"indices": [
+						77,
+						87
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VKMPgAHWp0",
+					"expanded_url": "https://www.instagram.com/p/Bu7TdltA_bo/",
+					"display_url": "instagram.com/p/Bu7TdltA_bo/",
+					"indices": [
+						88,
+						111
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1105625452582981600,
+					"id_str": "1105625452582981635",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/D1f4fcPW0AMpGAB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1f4fcPW0AMpGAB.jpg",
+					"url": "https://t.co/V8OstdBm5P",
+					"display_url": "pic.twitter.com/V8OstdBm5P",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1105625454042583040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1105625452582981600,
+					"id_str": "1105625452582981635",
+					"indices": [
+						112,
+						135
+					],
+					"media_url": "http://pbs.twimg.com/media/D1f4fcPW0AMpGAB.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1f4fcPW0AMpGAB.jpg",
+					"url": "https://t.co/V8OstdBm5P",
+					"display_url": "pic.twitter.com/V8OstdBm5P",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1105625454042583040/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7511,
+			"friends_count": 5156,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6434,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1662,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "500 days to go to the Tokyo 2020 Olympics! 🇸🇬⛵️💨🏄‍♀️ #roadtotokyo #tokyo2020 #SailforSG https://t.co/VKMPgAHWp0 https://t.co/V8OstdBm5P"
+	},
+	{
 		"created_at": "Tue Mar 12 00:30:03 +0000 2019",
 		"id": 1105264655411359700,
 		"id_str": "1105264655411359744",
