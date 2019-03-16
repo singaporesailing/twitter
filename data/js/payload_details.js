ab199965697d720540e2a475807c43d04611d46c
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-03-16 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1689,
-	"created_at": "Fri Mar 15 06:12:15 +0000 2019",
+	"tweets": 1690,
+	"created_at": "Sat Mar 16 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,10 +1,209 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Sat Mar 16 00:30:08 +0000 2019",
+		"id": 1106714227291631600,
+		"id_str": "1106714227291631623",
+		"full_text": "Feeling bored when there is no wind before a regatta? Simply grab some recycled paper and here are some things that you can do to free your boredom. Perhaps you will discover a secret talent that you have. 😆 #SailingSaturday #PaperOrigami https://t.co/vVM63s8yV4 https://t.co/nqEDCGahGH",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			262
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SailingSaturday",
+					"indices": [
+						208,
+						224
+					]
+				},
+				{
+					"text": "PaperOrigami",
+					"indices": [
+						225,
+						238
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/vVM63s8yV4",
+					"expanded_url": "http://artsycraftsydad.com/how-to-make-sailing-ship-paper-craft/",
+					"display_url": "artsycraftsydad.com/how-to-make-sa…",
+					"indices": [
+						239,
+						262
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1106714221566459900,
+					"id_str": "1106714221566459905",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/media/D1vWuIxXgAEI-sN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1vWuIxXgAEI-sN.jpg",
+					"url": "https://t.co/nqEDCGahGH",
+					"display_url": "pic.twitter.com/nqEDCGahGH",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1106714227291631623/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 860,
+							"h": 484,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 860,
+							"h": 484,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1106714221566459900,
+					"id_str": "1106714221566459905",
+					"indices": [
+						263,
+						286
+					],
+					"media_url": "http://pbs.twimg.com/media/D1vWuIxXgAEI-sN.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D1vWuIxXgAEI-sN.jpg",
+					"url": "https://t.co/nqEDCGahGH",
+					"display_url": "pic.twitter.com/nqEDCGahGH",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1106714227291631623/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 860,
+							"h": 484,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 860,
+							"h": 484,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7502,
+			"friends_count": 5183,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6439,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1666,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Feeling bored when there is no wind before a regatta? Simply grab some recycled paper and here are some things that you can do to free your boredom. Perhaps you will discover a secret talent that you have. 😆 #SailingSaturday #PaperOrigami https://t.co/vVM63s8yV4 https://t.co/nqEDCGahGH"
+	},
+	{
 		"created_at": "Thu Mar 14 22:04:25 +0000 2019",
 		"id": 1106315168068194300,
 		"id_str": "1106315168068194304",
-		"full_text": "We’re proud to have the support of homegrown smart wearables brand @actxa !⁣Sail on to check out the full Actxa collection at https://t.co/FWd3ho0RGH\n\n#BetterMovesBetterLives https://t.co/22ZswoJq7t",
+		"full_text": "We’re proud to have the support of homegrown smart wearables brand @actxa !Sail on to check out the full Actxa collection at https://t.co/FWd3ho0RGH\n\n#BetterMovesBetterLives https://t.co/22ZswoJq7t",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -273,7 +472,7 @@ Grailbird.data.tweets_2019_03 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "We’re proud to have the support of homegrown smart wearables brand @actxa !⁣Sail on to check out the full Actxa collection at https://t.co/FWd3ho0RGH\n\n#BetterMovesBetterLives https://t.co/22ZswoJq7t"
+		"text": "We’re proud to have the support of homegrown smart wearables brand @actxa !Sail on to check out the full Actxa collection at https://t.co/FWd3ho0RGH\n\n#BetterMovesBetterLives https://t.co/22ZswoJq7t"
 	},
 	{
 		"created_at": "Thu Mar 14 00:30:06 +0000 2019",
