b16b0d23697ec3a0c36c51d4965a1c1b3886a411
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-03-20 14:12:16 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1692,
-	"created_at": "Tue Mar 19 06:12:15 +0000 2019",
+	"tweets": 1693,
+	"created_at": "Wed Mar 20 06:12:15 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,205 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Wed Mar 20 00:30:11 +0000 2019",
+		"id": 1108163791278301200,
+		"id_str": "1108163791278301185",
+		"full_text": "Olympians are special beings that can do things normal humans cannot. Check out the link and let us know if you tried any! ✌️ #IAmStrong #WorkoutWednesdays \n\nP.S. Those under the age of 16 please do not try any of these without an adult's supervision. https://t.co/NZ7ILrwA0a https://t.co/K7brAg0LOW",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "IAmStrong",
+					"indices": [
+						126,
+						136
+					]
+				},
+				{
+					"text": "WorkoutWednesdays",
+					"indices": [
+						137,
+						155
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/NZ7ILrwA0a",
+					"expanded_url": "https://www.buzzfeed.com/lyapalater/athletes-training-who-prove-that-olympians-are-actually",
+					"display_url": "buzzfeed.com/lyapalater/ath…",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1108163787075522600,
+					"id_str": "1108163787075522560",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D2D9F9GWkAAcVzS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2D9F9GWkAAcVzS.jpg",
+					"url": "https://t.co/K7brAg0LOW",
+					"display_url": "pic.twitter.com/K7brAg0LOW",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1108163791278301185/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 625,
+							"h": 415,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 625,
+							"h": 415,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 625,
+							"h": 415,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1108163787075522600,
+					"id_str": "1108163787075522560",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D2D9F9GWkAAcVzS.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2D9F9GWkAAcVzS.jpg",
+					"url": "https://t.co/K7brAg0LOW",
+					"display_url": "pic.twitter.com/K7brAg0LOW",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1108163791278301185/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 625,
+							"h": 415,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 625,
+							"h": 415,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 625,
+							"h": 415,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7516,
+			"friends_count": 5157,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6439,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1669,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Olympians are special beings that can do things normal humans cannot. Check out the link and let us know if you tried any! ✌️ #IAmStrong #WorkoutWednesdays \n\nP.S. Those under the age of 16 please do not try any of these without an adult's supervision. https://t.co/NZ7ILrwA0a https://t.co/K7brAg0LOW"
+	},
+	{
 		"created_at": "Mon Mar 18 14:23:55 +0000 2019",
 		"id": 1107648833218060300,
 		"id_str": "1107648833218060288",
