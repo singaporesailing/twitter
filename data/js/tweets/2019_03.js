85e6b0ead4e83dfd4c6f764fae1b6db414072932
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-03-24 14:12:15 +0800

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Sun Mar 24 00:30:04 +0000 2019",
+		"id": 1109613312860475400,
+		"id_str": "1109613312860475392",
+		"full_text": "Everyone wants to raise awareness of the environmental and health impacts of single-use plastics. However, if you were to be given a chance to sail on a mission across the Atlantic with Emily Penn, how many of you will do it? #SustainabilitySunday https://t.co/u42YX9MIxG https://t.co/kkeBpS6Zh3",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SustainabilitySunday",
+					"indices": [
+						226,
+						247
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/u42YX9MIxG",
+					"expanded_url": "https://inews.co.uk/news/environment/300-women-wanted-for-a-round-the-world-sailing-mission-raising-awareness-of-plastic-pollution/",
+					"display_url": "inews.co.uk/news/environme…",
+					"indices": [
+						248,
+						271
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1109613308624228400,
+					"id_str": "1109613308624228354",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/D2YjbNqX0AIf05R.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2YjbNqX0AIf05R.jpg",
+					"url": "https://t.co/kkeBpS6Zh3",
+					"display_url": "pic.twitter.com/kkeBpS6Zh3",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1109613312860475392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 357,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1109613308624228400,
+					"id_str": "1109613308624228354",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/D2YjbNqX0AIf05R.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2YjbNqX0AIf05R.jpg",
+					"url": "https://t.co/kkeBpS6Zh3",
+					"display_url": "pic.twitter.com/kkeBpS6Zh3",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1109613312860475392/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 357,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 630,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7532,
+			"friends_count": 5220,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6442,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1681,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Everyone wants to raise awareness of the environmental and health impacts of single-use plastics. However, if you were to be given a chance to sail on a mission across the Atlantic with Emily Penn, how many of you will do it? #SustainabilitySunday https://t.co/u42YX9MIxG https://t.co/kkeBpS6Zh3"
+	},
+	{
 		"created_at": "Sat Mar 23 00:09:00 +0000 2019",
 		"id": 1109245626011799600,
 		"id_str": "1109245626011799552",
@@ -110,7 +302,7 @@ Grailbird.data.tweets_2019_03 =
 		"created_at": "Fri Mar 22 23:34:40 +0000 2019",
 		"id": 1109236984482914300,
 		"id_str": "1109236984482914304",
-		"full_text": "\"When we dance in the ocean....\"⁣\n😀💃⛵🌊⁣\n😀🕺⛵🌊⁣\n\nThanks again to our sponsors @actxa⁣\n⁣Thanks to our photographers⁣\n📷: @Haw_Lin \n📷: @Vaxzone⁣\n⁣\n#NationalYouthSailingChampionship2019 \n#sunshine #sunshinetherapy #beachvibes #endlesssummer #oceanvibes #vitaminsea https://t.co/6mDZdzIS1h",
+		"full_text": "\"When we dance in the ocean....\"\n😀💃⛵🌊\n😀🕺⛵🌊\n\nThanks again to our sponsors @actxa\nThanks to our photographers\n📷: @Haw_Lin \n📷: @Vaxzone\n\n#NationalYouthSailingChampionship2019 \n#sunshine #sunshinetherapy #beachvibes #endlesssummer #oceanvibes #vitaminsea https://t.co/6mDZdzIS1h",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -380,7 +572,7 @@ Grailbird.data.tweets_2019_03 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "\"When we dance in the ocean....\"⁣\n😀💃⛵🌊⁣\n😀🕺⛵🌊⁣\n\nThanks again to our sponsors @actxa⁣\n⁣Thanks to our photographers⁣\n📷: @Haw_Lin \n📷: @Vaxzone⁣\n⁣\n#NationalYouthSailingChampionship2019 \n#sunshine #sunshinetherapy #beachvibes #endlesssummer #oceanvibes #vitaminsea https://t.co/6mDZdzIS1h"
+		"text": "\"When we dance in the ocean....\"\n😀💃⛵🌊\n😀🕺⛵🌊\n\nThanks again to our sponsors @actxa\nThanks to our photographers\n📷: @Haw_Lin \n📷: @Vaxzone\n\n#NationalYouthSailingChampionship2019 \n#sunshine #sunshinetherapy #beachvibes #endlesssummer #oceanvibes #vitaminsea https://t.co/6mDZdzIS1h"
 	},
 	{
 		"created_at": "Fri Mar 22 18:50:31 +0000 2019",
