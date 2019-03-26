c2e065b1c322af2884fbc4e65cb37fa88ee122d9
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-03-26 14:12:16 +0800

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Tue Mar 26 00:30:04 +0000 2019",
+		"id": 1110338090432901100,
+		"id_str": "1110338090432901121",
+		"full_text": "What is considered healthy and what is considered unhealthy? There is a lot of controversy in nutrition and it often seems like people can't agree on anything. Here are the top 10 nutrition facts that almost everyone agrees on! #HealthyTipTuesday https://t.co/n6VuRSInrd https://t.co/WdqLWFv6SM",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			270
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "HealthyTipTuesday",
+					"indices": [
+						228,
+						246
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/n6VuRSInrd",
+					"expanded_url": "https://www.healthline.com/nutrition/top-10-nutrition-facts",
+					"display_url": "healthline.com/nutrition/top-…",
+					"indices": [
+						247,
+						270
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1110338086649675800,
+					"id_str": "1110338086649675778",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/D2i2m2NXQAIZ2IZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2i2m2NXQAIZ2IZ.jpg",
+					"url": "https://t.co/WdqLWFv6SM",
+					"display_url": "pic.twitter.com/WdqLWFv6SM",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1110338090432901121/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 732,
+							"h": 549,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 732,
+							"h": 549,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1110338086649675800,
+					"id_str": "1110338086649675778",
+					"indices": [
+						271,
+						294
+					],
+					"media_url": "http://pbs.twimg.com/media/D2i2m2NXQAIZ2IZ.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2i2m2NXQAIZ2IZ.jpg",
+					"url": "https://t.co/WdqLWFv6SM",
+					"display_url": "pic.twitter.com/WdqLWFv6SM",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1110338090432901121/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 510,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 732,
+							"h": 549,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 732,
+							"h": 549,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7531,
+			"friends_count": 5182,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6442,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1683,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "What is considered healthy and what is considered unhealthy? There is a lot of controversy in nutrition and it often seems like people can't agree on anything. Here are the top 10 nutrition facts that almost everyone agrees on! #HealthyTipTuesday https://t.co/n6VuRSInrd https://t.co/WdqLWFv6SM"
+	},
+	{
 		"created_at": "Sun Mar 24 00:30:04 +0000 2019",
 		"id": 1109613312860475400,
 		"id_str": "1109613312860475392",
