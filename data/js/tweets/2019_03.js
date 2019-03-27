896c80dd6291b28f7810c5345b1390fb4dcb7f4e
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-03-27 14:12:15 +0800

    Added 1 tweets

--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Tue Mar 26 11:23:37 +0000 2019",
+		"id": 1110502559867981800,
+		"id_str": "1110502559867981826",
+		"full_text": "Our Laser Standard sailor, Ryan Lo training hard for the Princess Sofia Regatta in Palma! Good luck Ryan! 💪💪🇸🇬⛵️⠀\n⠀\n#SailforSG #ItTakesAVillage #MakingWavesTogether #raisingchampions https://t.co/gHl0EAqpTe https://t.co/nLtf0ZgL8c",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			206
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SailforSG",
+					"indices": [
+						116,
+						126
+					]
+				},
+				{
+					"text": "ItTakesAVillage",
+					"indices": [
+						127,
+						143
+					]
+				},
+				{
+					"text": "MakingWavesTogether",
+					"indices": [
+						144,
+						164
+					]
+				},
+				{
+					"text": "raisingchampions",
+					"indices": [
+						165,
+						182
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/gHl0EAqpTe",
+					"expanded_url": "https://www.instagram.com/p/BveB0F8hF1P/",
+					"display_url": "instagram.com/p/BveB0F8hF1P/",
+					"indices": [
+						183,
+						206
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1110502558227939300,
+					"id_str": "1110502558227939330",
+					"indices": [
+						207,
+						230
+					],
+					"media_url": "http://pbs.twimg.com/media/D2lMMWlWwAIAVMo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2lMMWlWwAIAVMo.jpg",
+					"url": "https://t.co/nLtf0ZgL8c",
+					"display_url": "pic.twitter.com/nLtf0ZgL8c",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1110502559867981826/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 626,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 626,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 626,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1110502558227939300,
+					"id_str": "1110502558227939330",
+					"indices": [
+						207,
+						230
+					],
+					"media_url": "http://pbs.twimg.com/media/D2lMMWlWwAIAVMo.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D2lMMWlWwAIAVMo.jpg",
+					"url": "https://t.co/nLtf0ZgL8c",
+					"display_url": "pic.twitter.com/nLtf0ZgL8c",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1110502559867981826/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 626,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 626,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 626,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7536,
+			"friends_count": 5232,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6442,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1685,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 8,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our Laser Standard sailor, Ryan Lo training hard for the Princess Sofia Regatta in Palma! Good luck Ryan! 💪💪🇸🇬⛵️⠀\n⠀\n#SailforSG #ItTakesAVillage #MakingWavesTogether #raisingchampions https://t.co/gHl0EAqpTe https://t.co/nLtf0ZgL8c"
+	},
+	{
 		"created_at": "Tue Mar 26 00:30:04 +0000 2019",
 		"id": 1110338090432901100,
 		"id_str": "1110338090432901121",
