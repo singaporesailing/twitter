b8819739dd58f98fe216b00df7f12de03759bbc2
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-03-30 14:12:16 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1707,
-	"created_at": "Thu Mar 28 06:12:15 +0000 2019",
+	"tweets": 1708,
+	"created_at": "Sat Mar 30 06:12:15 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_03.js
+++ data/js/tweets/2019_03.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_03 = 
 [
 	{
+		"created_at": "Sat Mar 30 00:30:05 +0000 2019",
+		"id": 1111787644911001600,
+		"id_str": "1111787644911001600",
+		"full_text": "\"You're effectively playing chess on water and you're managing risk all the time,\" said Britain's Ian Walker, who is a double Olympic silver medalist and Volvo Ocean Race-winning skipper.\n\nIsn't that what makes sailing one of the best sports? 😍 https://t.co/74DZP1QC8V https://t.co/a9RJgghehy",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			268
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/74DZP1QC8V",
+					"expanded_url": "https://edition.cnn.com/2019/03/20/sport/sailing-leadership-tracy-edwards-ian-walker-olympics-business-main-sail-spt-intl/index.html?fbclid=IwAR1ABsGuX-8Ag2amc5d5VzSIvJnzglI_7RylaX7_0LCttgXbzbflcZmxgL0",
+					"display_url": "edition.cnn.com/2019/03/20/spo…",
+					"indices": [
+						245,
+						268
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1111787639991136300,
+					"id_str": "1111787639991136256",
+					"indices": [
+						269,
+						292
+					],
+					"media_url": "http://pbs.twimg.com/media/D23c99NXcAANa6A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D23c99NXcAANa6A.jpg",
+					"url": "https://t.co/a9RJgghehy",
+					"display_url": "pic.twitter.com/a9RJgghehy",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1111787644911001600/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1100,
+							"h": 619,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1100,
+							"h": 619,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1111787639991136300,
+					"id_str": "1111787639991136256",
+					"indices": [
+						269,
+						292
+					],
+					"media_url": "http://pbs.twimg.com/media/D23c99NXcAANa6A.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D23c99NXcAANa6A.jpg",
+					"url": "https://t.co/a9RJgghehy",
+					"display_url": "pic.twitter.com/a9RJgghehy",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1111787644911001600/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1100,
+							"h": 619,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1100,
+							"h": 619,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7540,
+			"friends_count": 5228,
+			"listed_count": 42,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6442,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1689,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"You're effectively playing chess on water and you're managing risk all the time,\" said Britain's Ian Walker, who is a double Olympic silver medalist and Volvo Ocean Race-winning skipper.\n\nIsn't that what makes sailing one of the best sports? 😍 https://t.co/74DZP1QC8V https://t.co/a9RJgghehy"
+	},
+	{
 		"created_at": "Wed Mar 27 14:00:47 +0000 2019",
 		"id": 1110904501676982300,
 		"id_str": "1110904501676982273",
