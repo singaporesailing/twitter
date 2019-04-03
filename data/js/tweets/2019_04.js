3d88b6eb12be4f3b907b508782caadd77398c5dd
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-04-03 14:12:15 +0800

    Added 1 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,141 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Wed Apr 03 00:30:03 +0000 2019",
+		"id": 1113237187649261600,
+		"id_str": "1113237187649261569",
+		"full_text": "Are you a new runner? Have you decided to start running to improve your fitness? Before lacing up your shoes, check out these 8 extremely useful running tips for beginners! 🏃 #BetterStamina #IAmStronger #WorkoutWednesday #WellnessWednesday\n https://t.co/aGaeQqlq4G",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			264
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "BetterStamina",
+					"indices": [
+						175,
+						189
+					]
+				},
+				{
+					"text": "IAmStronger",
+					"indices": [
+						190,
+						202
+					]
+				},
+				{
+					"text": "WorkoutWednesday",
+					"indices": [
+						203,
+						220
+					]
+				},
+				{
+					"text": "WellnessWednesday",
+					"indices": [
+						221,
+						239
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/aGaeQqlq4G",
+					"expanded_url": "https://www.runtastic.com/blog/en/8-extremely-useful-running-tips-for-beginners/",
+					"display_url": "runtastic.com/blog/en/8-extr…",
+					"indices": [
+						241,
+						264
+					]
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7535,
+			"friends_count": 5164,
+			"listed_count": 45,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6442,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1693,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Are you a new runner? Have you decided to start running to improve your fitness? Before lacing up your shoes, check out these 8 extremely useful running tips for beginners! 🏃 #BetterStamina #IAmStronger #WorkoutWednesday #WellnessWednesday\n https://t.co/aGaeQqlq4G"
+	},
+	{
 		"created_at": "Mon Apr 01 10:23:49 +0000 2019",
 		"id": 1112661841400991700,
 		"id_str": "1112661841400991744",
