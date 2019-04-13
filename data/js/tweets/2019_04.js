0f51d9ddc6f4ac2e921ae2fd34f925dc4632affe
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-04-13 14:12:15 +0800

    Added 1 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sat Apr 13 00:30:04 +0000 2019",
+		"id": 1116861072005369900,
+		"id_str": "1116861072005369856",
+		"full_text": "\"The kind of confidence and life lessons that she can draw from (sailing) to help her navigate life’s beautiful, bumpy waters.\"\n- Lea Maxwell\n\nRead this blog and we would love to know if you agree with any of the lessons mentioned. #SailingSaturday ⛵☀️ https://t.co/6OGYxrkYaG https://t.co/TmCGDUbARb",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			276
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SailingSaturday",
+					"indices": [
+						232,
+						248
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/6OGYxrkYaG",
+					"expanded_url": "https://thriveglobal.com/stories/what-your-daughter-learns-when-she-learns-to-sail/?fbclid=IwAR104RIF-GQWX77aHtuH8B8hNCptiJRkyzuLG8g_nRV1aputcN8OJANuZ2o",
+					"display_url": "thriveglobal.com/stories/what-y…",
+					"indices": [
+						253,
+						276
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1116861067940986900,
+					"id_str": "1116861067940986880",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/D3_jOVIWAAALojP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3_jOVIWAAALojP.jpg",
+					"url": "https://t.co/TmCGDUbARb",
+					"display_url": "pic.twitter.com/TmCGDUbARb",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1116861072005369856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 355,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 522,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 522,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1116861067940986900,
+					"id_str": "1116861067940986880",
+					"indices": [
+						277,
+						300
+					],
+					"media_url": "http://pbs.twimg.com/media/D3_jOVIWAAALojP.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D3_jOVIWAAALojP.jpg",
+					"url": "https://t.co/TmCGDUbARb",
+					"display_url": "pic.twitter.com/TmCGDUbARb",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1116861072005369856/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 355,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1000,
+							"h": 522,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1000,
+							"h": 522,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7535,
+			"friends_count": 5234,
+			"listed_count": 45,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6441,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1701,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"The kind of confidence and life lessons that she can draw from (sailing) to help her navigate life’s beautiful, bumpy waters.\"\n- Lea Maxwell\n\nRead this blog and we would love to know if you agree with any of the lessons mentioned. #SailingSaturday ⛵☀️ https://t.co/6OGYxrkYaG https://t.co/TmCGDUbARb"
+	},
+	{
 		"created_at": "Thu Apr 11 00:30:06 +0000 2019",
 		"id": 1116136305887518700,
 		"id_str": "1116136305887518720",
