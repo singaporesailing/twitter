e9d4ce90151d7f186ed1a0294dbe51ef9617d34b
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-04-28 14:12:15 +0800

    Added 1 tweets

--- data/js/tweets/2019_04.js
+++ data/js/tweets/2019_04.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_04 = 
 [
 	{
+		"created_at": "Sat Apr 27 11:23:51 +0000 2019",
+		"id": 1122099034112647200,
+		"id_str": "1122099034112647168",
+		"full_text": "Thank you to The Smart Local for this article coverage on our DBS Sailing at the bay programme. If you have yet to try it out, the programme is free for all those from the age of 7 to 70.⠀\n⠀\n\"See Singapore’s CBD in a different light by sailing around… https://t.co/tVyRKEvSXp https://t.co/ZlnONDYvn6",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/tVyRKEvSXp",
+					"expanded_url": "https://www.instagram.com/p/Bwwaky9hNex/",
+					"display_url": "instagram.com/p/Bwwaky9hNex/",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1122099033252737000,
+					"id_str": "1122099033252737025",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D5J_ICfWsAEairD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5J_ICfWsAEairD.jpg",
+					"url": "https://t.co/ZlnONDYvn6",
+					"display_url": "pic.twitter.com/ZlnONDYvn6",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1122099034112647168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1122099033252737000,
+					"id_str": "1122099033252737025",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D5J_ICfWsAEairD.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5J_ICfWsAEairD.jpg",
+					"url": "https://t.co/ZlnONDYvn6",
+					"display_url": "pic.twitter.com/ZlnONDYvn6",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1122099034112647168/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7549,
+			"friends_count": 5283,
+			"listed_count": 45,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6437,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1721,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 0,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Thank you to The Smart Local for this article coverage on our DBS Sailing at the bay programme. If you have yet to try it out, the programme is free for all those from the age of 7 to 70.⠀\n⠀\n\"See Singapore’s CBD in a different light by sailing around… https://t.co/tVyRKEvSXp https://t.co/ZlnONDYvn6"
+	},
+	{
 		"created_at": "Sat Apr 27 00:30:14 +0000 2019",
 		"id": 1121934545056612400,
 		"id_str": "1121934545056612353",
