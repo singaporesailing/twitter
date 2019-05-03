eeef83dc22b4c6cd129d4fcb69286705b098278a
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-05-03 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1733,
-	"created_at": "Thu May 02 06:12:15 +0000 2019",
+	"tweets": 1734,
+	"created_at": "Fri May 03 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 03 00:30:04 +0000 2019",
+		"id": 1124108829384425500,
+		"id_str": "1124108829384425474",
+		"full_text": "\"Visualize your day going perfect in detail. Commit to the habit for 30 days and your life will change.”\n- Steve Weatherford, former NFL Super Bowl champion\n\nDo not belittle the simple power of visualization, try it today! #FeelGoodFriday 😄 https://t.co/j0glLs2QHt https://t.co/VBxuFGMLcG",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			264
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "FeelGoodFriday",
+					"indices": [
+						223,
+						238
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/j0glLs2QHt",
+					"expanded_url": "https://www.success.com/the-power-of-positive-thinking/",
+					"display_url": "success.com/the-power-of-p…",
+					"indices": [
+						241,
+						264
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1124108825462751200,
+					"id_str": "1124108825462751234",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/D5mjBV6WwAIyHV9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5mjBV6WwAIyHV9.jpg",
+					"url": "https://t.co/VBxuFGMLcG",
+					"display_url": "pic.twitter.com/VBxuFGMLcG",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1124108829384425474/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 582,
+							"h": 424,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 582,
+							"h": 424,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 582,
+							"h": 424,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1124108825462751200,
+					"id_str": "1124108825462751234",
+					"indices": [
+						265,
+						288
+					],
+					"media_url": "http://pbs.twimg.com/media/D5mjBV6WwAIyHV9.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5mjBV6WwAIyHV9.jpg",
+					"url": "https://t.co/VBxuFGMLcG",
+					"display_url": "pic.twitter.com/VBxuFGMLcG",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1124108829384425474/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 582,
+							"h": 424,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 582,
+							"h": 424,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 582,
+							"h": 424,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7539,
+			"friends_count": 5280,
+			"listed_count": 45,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6436,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1729,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"Visualize your day going perfect in detail. Commit to the habit for 30 days and your life will change.”\n- Steve Weatherford, former NFL Super Bowl champion\n\nDo not belittle the simple power of visualization, try it today! #FeelGoodFriday 😄 https://t.co/j0glLs2QHt https://t.co/VBxuFGMLcG"
+	},
+	{
 		"created_at": "Wed May 01 14:00:47 +0000 2019",
 		"id": 1123588075253317600,
 		"id_str": "1123588075253317637",
