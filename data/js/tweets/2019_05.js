c92b308cfed3382e20e56ba54ebb45c5a0d02b10
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-05-04 14:12:15 +0800

    Added 1 tweets

--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,219 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 03 15:24:03 +0000 2019",
+		"id": 1124333807308558300,
+		"id_str": "1124333807308558337",
+		"full_text": "The weekend is just round the corner, are you excited to sail? ⛵️⠀\n⠀\n#SailforSG #ItTakesAVillage #MakingWavesTogether #raisingchampions https://t.co/VQnjr6zZq3 https://t.co/1KPiXopvLt",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			159
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SailforSG",
+					"indices": [
+						69,
+						79
+					]
+				},
+				{
+					"text": "ItTakesAVillage",
+					"indices": [
+						80,
+						96
+					]
+				},
+				{
+					"text": "MakingWavesTogether",
+					"indices": [
+						97,
+						117
+					]
+				},
+				{
+					"text": "raisingchampions",
+					"indices": [
+						118,
+						135
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VQnjr6zZq3",
+					"expanded_url": "https://www.instagram.com/p/BxAPHy4Bjkm/",
+					"display_url": "instagram.com/p/BxAPHy4Bjkm/",
+					"indices": [
+						136,
+						159
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1124333806310318100,
+					"id_str": "1124333806310318080",
+					"indices": [
+						160,
+						183
+					],
+					"media_url": "http://pbs.twimg.com/media/D5pvo84W4AAoIey.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5pvo84W4AAoIey.jpg",
+					"url": "https://t.co/1KPiXopvLt",
+					"display_url": "pic.twitter.com/1KPiXopvLt",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1124333807308558337/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1124333806310318100,
+					"id_str": "1124333806310318080",
+					"indices": [
+						160,
+						183
+					],
+					"media_url": "http://pbs.twimg.com/media/D5pvo84W4AAoIey.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D5pvo84W4AAoIey.jpg",
+					"url": "https://t.co/1KPiXopvLt",
+					"display_url": "pic.twitter.com/1KPiXopvLt",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1124333807308558337/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7537,
+			"friends_count": 5278,
+			"listed_count": 45,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6436,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1730,
+			"lang": "en",
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 1,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "The weekend is just round the corner, are you excited to sail? ⛵️⠀\n⠀\n#SailforSG #ItTakesAVillage #MakingWavesTogether #raisingchampions https://t.co/VQnjr6zZq3 https://t.co/1KPiXopvLt"
+	},
+	{
 		"created_at": "Fri May 03 00:30:04 +0000 2019",
 		"id": 1124108829384425500,
 		"id_str": "1124108829384425474",
