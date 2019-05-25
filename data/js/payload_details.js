6a84dc4c19751dbc5e6441e61698c62293059283
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-05-25 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1749,
-	"created_at": "Fri May 24 06:12:15 +0000 2019",
+	"tweets": 1750,
+	"created_at": "Sat May 25 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Sat May 25 00:30:06 +0000 2019",
+		"id": 1132081371177017300,
+		"id_str": "1132081371177017347",
+		"full_text": "\"But the most amazing feeling, was the feeling of freedom, the feeling that I felt when we hoisted her sails.\"\n- Ellen MacArthur, record holder solo long-distance yachtswoman\n\nDoes anybody get the same feeling of freedom as her too? 🙋 #SailingSaturday https://t.co/4Hq3hhTp5x https://t.co/2EMJbim096",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SailingSaturday",
+					"indices": [
+						235,
+						251
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/4Hq3hhTp5x",
+					"expanded_url": "https://www.ted.com/talks/dame_ellen_macarthur_the_surprising_thing_i_learned_sailing_solo_around_the_world?language=en#t-78893",
+					"display_url": "ted.com/talks/dame_ell…",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1132081367091744800,
+					"id_str": "1132081367091744774",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D7X2AV5XkAYYM-u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7X2AV5XkAYYM-u.jpg",
+					"url": "https://t.co/2EMJbim096",
+					"display_url": "pic.twitter.com/2EMJbim096",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1132081371177017347/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1132081367091744800,
+					"id_str": "1132081367091744774",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D7X2AV5XkAYYM-u.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D7X2AV5XkAYYM-u.jpg",
+					"url": "https://t.co/2EMJbim096",
+					"display_url": "pic.twitter.com/2EMJbim096",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1132081371177017347/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 675,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7489,
+			"friends_count": 5274,
+			"listed_count": 45,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6432,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1754,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"But the most amazing feeling, was the feeling of freedom, the feeling that I felt when we hoisted her sails.\"\n- Ellen MacArthur, record holder solo long-distance yachtswoman\n\nDoes anybody get the same feeling of freedom as her too? 🙋 #SailingSaturday https://t.co/4Hq3hhTp5x https://t.co/2EMJbim096"
+	},
+	{
 		"created_at": "Thu May 23 12:21:43 +0000 2019",
 		"id": 1131535679224275000,
 		"id_str": "1131535679224274946",
