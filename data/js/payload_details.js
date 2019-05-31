58dc926b49e1c3e2aa35e288e28e0a145adfe3b7
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-05-31 14:12:16 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1753,
-	"created_at": "Thu May 30 06:12:15 +0000 2019",
+	"tweets": 1754,
+	"created_at": "Fri May 31 06:12:15 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_05.js
+++ data/js/tweets/2019_05.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_05 = 
 [
 	{
+		"created_at": "Fri May 31 00:30:06 +0000 2019",
+		"id": 1134255698613850100,
+		"id_str": "1134255698613850112",
+		"full_text": "Something crazy happens when you start to get older. You start to get out of bed and think, “Wow, I should actually accomplish something interesting and fulfilling today.” Read on for the 10 simple ways! Have a great weekend everyone!! 😄 https://t.co/Y1p04IMIOW https://t.co/YyFGsq8dJV",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			261
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Y1p04IMIOW",
+					"expanded_url": "https://www.lifehack.org/articles/lifestyle/10-simple-ways-make-you-feel-good-instantly.html",
+					"display_url": "lifehack.org/articles/lifes…",
+					"indices": [
+						238,
+						261
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1134255694570565600,
+					"id_str": "1134255694570565632",
+					"indices": [
+						262,
+						285
+					],
+					"media_url": "http://pbs.twimg.com/media/D72vi29XsAAMoSA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D72vi29XsAAMoSA.jpg",
+					"url": "https://t.co/YyFGsq8dJV",
+					"display_url": "pic.twitter.com/YyFGsq8dJV",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1134255698613850112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1134255694570565600,
+					"id_str": "1134255694570565632",
+					"indices": [
+						262,
+						285
+					],
+					"media_url": "http://pbs.twimg.com/media/D72vi29XsAAMoSA.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D72vi29XsAAMoSA.jpg",
+					"url": "https://t.co/YyFGsq8dJV",
+					"display_url": "pic.twitter.com/YyFGsq8dJV",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1134255698613850112/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 680,
+							"h": 383,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 960,
+							"h": 540,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7481,
+			"friends_count": 5272,
+			"listed_count": 45,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6431,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1760,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Something crazy happens when you start to get older. You start to get out of bed and think, “Wow, I should actually accomplish something interesting and fulfilling today.” Read on for the 10 simple ways! Have a great weekend everyone!! 😄 https://t.co/Y1p04IMIOW https://t.co/YyFGsq8dJV"
+	},
+	{
 		"created_at": "Wed May 29 14:00:38 +0000 2019",
 		"id": 1133734898496417800,
 		"id_str": "1133734898496417792",
