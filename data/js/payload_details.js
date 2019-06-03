e1ee8dae51f754b34d2060298a20b37897f307d3
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-06-03 14:12:16 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1756,
-	"created_at": "Sun Jun 02 06:12:14 +0000 2019",
+	"tweets": 1757,
+	"created_at": "Mon Jun 03 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Sun Jun 02 12:45:25 +0000 2019",
+		"id": 1135165521765261300,
+		"id_str": "1135165521765261315",
+		"full_text": "Our 49er FX sailors Cecilia (@cecilialowruiqi) and Kimberly (@kimmy_3129) chasing their Olympic dream (Tokyo 2020) 💪🇸🇬!⠀\n⠀\n[Repost]⠀\nAiming High ⠀\nBora➡️🇯🇵 2020⠀\n@singaporesailingteam | @singaporesportsinstutute |@gottifredimaffioli | @garminsg |… https://t.co/n75dKc5oLm https://t.co/Tegd4jNoCs",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "garminsg",
+					"name": "Garmin Singapore",
+					"id": 3064011488,
+					"id_str": "3064011488",
+					"indices": [
+						235,
+						244
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/n75dKc5oLm",
+					"expanded_url": "https://www.instagram.com/p/ByNOPtyg8zb/",
+					"display_url": "instagram.com/p/ByNOPtyg8zb/",
+					"indices": [
+						248,
+						271
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1135165520821542900,
+					"id_str": "1135165520821542912",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/D8DrBt3W4AAhMgR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8DrBt3W4AAhMgR.jpg",
+					"url": "https://t.co/Tegd4jNoCs",
+					"display_url": "pic.twitter.com/Tegd4jNoCs",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1135165521765261315/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 479,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 479,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 479,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1135165520821542900,
+					"id_str": "1135165520821542912",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/D8DrBt3W4AAhMgR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D8DrBt3W4AAhMgR.jpg",
+					"url": "https://t.co/Tegd4jNoCs",
+					"display_url": "pic.twitter.com/Tegd4jNoCs",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1135165521765261315/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 479,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 479,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 479,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7473,
+			"friends_count": 5272,
+			"listed_count": 45,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6429,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1763,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our 49er FX sailors Cecilia (@cecilialowruiqi) and Kimberly (@kimmy_3129) chasing their Olympic dream (Tokyo 2020) 💪🇸🇬!⠀\n⠀\n[Repost]⠀\nAiming High ⠀\nBora➡️🇯🇵 2020⠀\n@singaporesailingteam | @singaporesportsinstutute |@gottifredimaffioli | @garminsg |… https://t.co/n75dKc5oLm https://t.co/Tegd4jNoCs"
+	},
+	{
 		"created_at": "Sun Jun 02 00:30:03 +0000 2019",
 		"id": 1134980461665685500,
 		"id_str": "1134980461665685506",
