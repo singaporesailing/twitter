d7890c65b08a69c74c4cc60f8f0bf729b96736f2
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-06-24 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1774,
-	"created_at": "Sun Jun 23 06:12:14 +0000 2019",
+	"tweets": 1775,
+	"created_at": "Mon Jun 24 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_06.js
+++ data/js/tweets/2019_06.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_06 = 
 [
 	{
+		"created_at": "Mon Jun 24 00:30:03 +0000 2019",
+		"id": 1142952994683674600,
+		"id_str": "1142952994683674624",
+		"full_text": "\"I am the greatest. I said that before I even knew I was.”\n- Muhammad Ali, American professional boxer\n\nIt’s the real, genuine inner belief in yourself and your own abilities that will propel you into a higher league. You can do it 💪 #MotivationMonday https://t.co/3BztcmoXTj https://t.co/eKEkGCzk3h",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "MotivationMonday",
+					"indices": [
+						234,
+						251
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/3BztcmoXTj",
+					"expanded_url": "https://www.success.com/how-to-gain-confidence-and-become-the-greatest/",
+					"display_url": "success.com/how-to-gain-co…",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1142952990694944800,
+					"id_str": "1142952990694944768",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D9yVsH1XsAAw5z8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D9yVsH1XsAAw5z8.jpg",
+					"url": "https://t.co/eKEkGCzk3h",
+					"display_url": "pic.twitter.com/eKEkGCzk3h",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1142952994683674624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 582,
+							"h": 424,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 582,
+							"h": 424,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 582,
+							"h": 424,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1142952990694944800,
+					"id_str": "1142952990694944768",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D9yVsH1XsAAw5z8.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D9yVsH1XsAAw5z8.jpg",
+					"url": "https://t.co/eKEkGCzk3h",
+					"display_url": "pic.twitter.com/eKEkGCzk3h",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1142952994683674624/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 582,
+							"h": 424,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 582,
+							"h": 424,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 582,
+							"h": 424,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "Singapore Sailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7438,
+			"friends_count": 5261,
+			"listed_count": 45,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6426,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1787,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "\"I am the greatest. I said that before I even knew I was.”\n- Muhammad Ali, American professional boxer\n\nIt’s the real, genuine inner belief in yourself and your own abilities that will propel you into a higher league. You can do it 💪 #MotivationMonday https://t.co/3BztcmoXTj https://t.co/eKEkGCzk3h"
+	},
+	{
 		"created_at": "Sat Jun 22 14:19:49 +0000 2019",
 		"id": 1142437037049561100,
 		"id_str": "1142437037049561088",
