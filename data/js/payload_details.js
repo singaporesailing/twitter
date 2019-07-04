a0b2dcd5be06cc280ab946602b1231f7a4c9e9ec
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-07-04 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1782,
-	"created_at": "Tue Jul 02 06:12:14 +0000 2019",
+	"tweets": 1783,
+	"created_at": "Thu Jul 04 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,198 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Thu Jul 04 00:30:04 +0000 2019",
+		"id": 1146576877102018600,
+		"id_str": "1146576877102018560",
+		"full_text": "We are always thankful for the things and people around us. But how often do we feel thankful for ourselves? Self-appreciation is the foundation of a happy life. Without it, you can’t build a bigger you. 💛 #ThankfulThursday https://t.co/sUIzrEsaZp https://t.co/DETf3ujxUv",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			247
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "ThankfulThursday",
+					"indices": [
+						206,
+						223
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/sUIzrEsaZp",
+					"expanded_url": "https://medium.com/personal-growth/self-appreciation-is-the-foundation-of-life-c44af9189040",
+					"display_url": "medium.com/personal-growt…",
+					"indices": [
+						224,
+						247
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1146576872932872200,
+					"id_str": "1146576872932872193",
+					"indices": [
+						248,
+						271
+					],
+					"media_url": "http://pbs.twimg.com/media/D-l1l1LXkAE9L_r.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-l1l1LXkAE9L_r.jpg",
+					"url": "https://t.co/DETf3ujxUv",
+					"display_url": "pic.twitter.com/DETf3ujxUv",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1146576877102018560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 730,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 414,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 730,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1146576872932872200,
+					"id_str": "1146576872932872193",
+					"indices": [
+						248,
+						271
+					],
+					"media_url": "http://pbs.twimg.com/media/D-l1l1LXkAE9L_r.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-l1l1LXkAE9L_r.jpg",
+					"url": "https://t.co/DETf3ujxUv",
+					"display_url": "pic.twitter.com/DETf3ujxUv",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1146576877102018560/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 1200,
+							"h": 730,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 680,
+							"h": 414,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 1200,
+							"h": 730,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://app.agorapulse.com\" rel=\"nofollow\">AgoraPulse Manager</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "SingaporeSailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7416,
+			"friends_count": 5255,
+			"listed_count": 45,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6423,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1796,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 7,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "We are always thankful for the things and people around us. But how often do we feel thankful for ourselves? Self-appreciation is the foundation of a happy life. Without it, you can’t build a bigger you. 💛 #ThankfulThursday https://t.co/sUIzrEsaZp https://t.co/DETf3ujxUv"
+	},
+	{
 		"created_at": "Mon Jul 01 12:26:09 +0000 2019",
 		"id": 1145669923210956800,
 		"id_str": "1145669923210956805",
