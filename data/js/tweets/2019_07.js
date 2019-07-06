a574502c6e53f3d26247e59113d2099eba79489c
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-07-06 14:12:15 +0800

    Added 1 tweets

--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Fri Jul 05 15:04:47 +0000 2019",
+		"id": 1147159395735941100,
+		"id_str": "1147159395735941120",
+		"full_text": "Apart from the usual sailors you see on our Instagram, we also have sailors from different Universities representing Singapore in the Napoli 2019 Summer Universiade held in Naples, Italy 🇮🇹!⠀\n⠀\nThey will be competing in the RS21 class against other s… https://t.co/sDS9uXNOvI https://t.co/vHXcwfqeTn",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/sDS9uXNOvI",
+					"expanded_url": "https://www.instagram.com/p/BziVaZRAnAE/",
+					"display_url": "instagram.com/p/BziVaZRAnAE/",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1147159394657980400,
+					"id_str": "1147159394657980416",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D-uHZDtXUAAiG6M.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-uHZDtXUAAiG6M.jpg",
+					"url": "https://t.co/vHXcwfqeTn",
+					"display_url": "pic.twitter.com/vHXcwfqeTn",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1147159395735941120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1147159394657980400,
+					"id_str": "1147159394657980416",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/D-uHZDtXUAAiG6M.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D-uHZDtXUAAiG6M.jpg",
+					"url": "https://t.co/vHXcwfqeTn",
+					"display_url": "pic.twitter.com/vHXcwfqeTn",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1147159395735941120/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 480,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "SingaporeSailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7410,
+			"friends_count": 5254,
+			"listed_count": 44,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6422,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1797,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 3,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Apart from the usual sailors you see on our Instagram, we also have sailors from different Universities representing Singapore in the Napoli 2019 Summer Universiade held in Naples, Italy 🇮🇹!⠀\n⠀\nThey will be competing in the RS21 class against other s… https://t.co/sDS9uXNOvI https://t.co/vHXcwfqeTn"
+	},
+	{
 		"created_at": "Thu Jul 04 00:30:04 +0000 2019",
 		"id": 1146576877102018600,
 		"id_str": "1146576877102018560",
