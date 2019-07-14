29b830d27c5136a0a86c1fb2a43d4fc969651bc5
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-07-14 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1790,
-	"created_at": "Sat Jul 13 06:12:14 +0000 2019",
+	"tweets": 1791,
+	"created_at": "Sun Jul 14 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_07.js
+++ data/js/tweets/2019_07.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_07 = 
 [
 	{
+		"created_at": "Sat Jul 13 14:19:11 +0000 2019",
+		"id": 1150047021727924200,
+		"id_str": "1150047021727924228",
+		"full_text": "In the recently concluded 2019 Optimist European Championship held at Crozon-Morgat, France 🇫🇷, our country was represented by 4 young sailors 🇸🇬🇸🇬⠀\n⠀\nIn the girls category 💁‍♀️, we have Keira Caryle who came in 47th place and teammate Elisha Lim coming … https://t.co/Mfyi4c8WPb https://t.co/8sFuALVKd2",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/Mfyi4c8WPb",
+					"expanded_url": "https://www.instagram.com/p/Bz24d0sgqiu/",
+					"display_url": "instagram.com/p/Bz24d0sgqiu/",
+					"indices": [
+						256,
+						279
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1150047020683542500,
+					"id_str": "1150047020683542528",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/D_XJrA7WwAAjVo5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D_XJrA7WwAAjVo5.jpg",
+					"url": "https://t.co/8sFuALVKd2",
+					"display_url": "pic.twitter.com/8sFuALVKd2",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1150047021727924228/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1150047020683542500,
+					"id_str": "1150047020683542528",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/D_XJrA7WwAAjVo5.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/D_XJrA7WwAAjVo5.jpg",
+					"url": "https://t.co/8sFuALVKd2",
+					"display_url": "pic.twitter.com/8sFuALVKd2",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1150047021727924228/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 544,
+							"h": 680,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 800,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "SingaporeSailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7402,
+			"friends_count": 5250,
+			"listed_count": 44,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6421,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1804,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "In the recently concluded 2019 Optimist European Championship held at Crozon-Morgat, France 🇫🇷, our country was represented by 4 young sailors 🇸🇬🇸🇬⠀\n⠀\nIn the girls category 💁‍♀️, we have Keira Caryle who came in 47th place and teammate Elisha Lim coming … https://t.co/Mfyi4c8WPb https://t.co/8sFuALVKd2"
+	},
+	{
 		"created_at": "Fri Jul 12 16:18:32 +0000 2019",
 		"id": 1149714669168484400,
 		"id_str": "1149714669168484356",
