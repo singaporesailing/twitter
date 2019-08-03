8d89d965746102656be127360df50fb939f7c0f9
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-08-03 14:12:15 +0800

    Added 1 tweets

--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,190 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Fri Aug 02 15:31:49 +0000 2019",
+		"id": 1157313059708387300,
+		"id_str": "1157313059708387328",
+		"full_text": "Today is the first day of the Pesta Sukan Regatta 2019!\n\nRacing kicked off with medium to strong winds today as the sailors raced hard 💪\n\nThere will be many exciting activities in store tomorrow at our National Sailing Centre so do come down and join… https://t.co/X7NSUbp5tn https://t.co/0GOIEQkdLg",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			275
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/X7NSUbp5tn",
+					"expanded_url": "https://www.instagram.com/p/B0qik03ADwh/",
+					"display_url": "instagram.com/p/B0qik03ADwh/",
+					"indices": [
+						252,
+						275
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1157313058747928600,
+					"id_str": "1157313058747928576",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EA-aGGCXUAAHaDR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA-aGGCXUAAHaDR.jpg",
+					"url": "https://t.co/0GOIEQkdLg",
+					"display_url": "pic.twitter.com/0GOIEQkdLg",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1157313059708387328/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1157313058747928600,
+					"id_str": "1157313058747928576",
+					"indices": [
+						276,
+						299
+					],
+					"media_url": "http://pbs.twimg.com/media/EA-aGGCXUAAHaDR.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EA-aGGCXUAAHaDR.jpg",
+					"url": "https://t.co/0GOIEQkdLg",
+					"display_url": "pic.twitter.com/0GOIEQkdLg",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1157313059708387328/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "SingaporeSailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7376,
+			"friends_count": 5248,
+			"listed_count": 44,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6418,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1813,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 6,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Today is the first day of the Pesta Sukan Regatta 2019!\n\nRacing kicked off with medium to strong winds today as the sailors raced hard 💪\n\nThere will be many exciting activities in store tomorrow at our National Sailing Centre so do come down and join… https://t.co/X7NSUbp5tn https://t.co/0GOIEQkdLg"
+	},
+	{
 		"created_at": "Thu Aug 01 17:31:22 +0000 2019",
 		"id": 1156980755253383200,
 		"id_str": "1156980755253383168",
