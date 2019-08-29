1946776d3805174e0797e98dcc13404edbbab490
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-08-29 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1808,
-	"created_at": "Sat Aug 24 06:12:14 +0000 2019",
+	"tweets": 1809,
+	"created_at": "Thu Aug 29 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_08.js
+++ data/js/tweets/2019_08.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2019_08 = 
 [
 	{
+		"created_at": "Wed Aug 28 15:12:42 +0000 2019",
+		"id": 1166730330348408800,
+		"id_str": "1166730330348408832",
+		"full_text": "Our sailors Ryan (Laser men), Kimberly and Cecilia &amp; Griselda and Olivia (49er FX), Justin and Denise (Nacra 17) are currently in Enoshima competing in the Hempel World Cup Series organised by @worldsailing! ⠀\n⠀\nIt is currently the second day of raci… https://t.co/BHnhrYAIDb https://t.co/OODIwu97tO",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "worldsailing",
+					"name": "World Sailing 🌎⛵️",
+					"id": 39973326,
+					"id_str": "39973326",
+					"indices": [
+						197,
+						210
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/BHnhrYAIDb",
+					"expanded_url": "https://www.instagram.com/p/B1tQvIzgzLu/",
+					"display_url": "instagram.com/p/B1tQvIzgzLu/",
+					"indices": [
+						256,
+						279
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1166730329111105500,
+					"id_str": "1166730329111105536",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/EDEPDX9XkAAtAXL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDEPDX9XkAAtAXL.jpg",
+					"url": "https://t.co/OODIwu97tO",
+					"display_url": "pic.twitter.com/OODIwu97tO",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1166730330348408832/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 387,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 387,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 387,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1166730329111105500,
+					"id_str": "1166730329111105536",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/EDEPDX9XkAAtAXL.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EDEPDX9XkAAtAXL.jpg",
+					"url": "https://t.co/OODIwu97tO",
+					"display_url": "pic.twitter.com/OODIwu97tO",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1166730330348408832/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 387,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 387,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 387,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "SingaporeSailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7326,
+			"friends_count": 5223,
+			"listed_count": 44,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6415,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1823,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 3,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our sailors Ryan (Laser men), Kimberly and Cecilia &amp; Griselda and Olivia (49er FX), Justin and Denise (Nacra 17) are currently in Enoshima competing in the Hempel World Cup Series organised by @worldsailing! ⠀\n⠀\nIt is currently the second day of raci… https://t.co/BHnhrYAIDb https://t.co/OODIwu97tO"
+	},
+	{
 		"created_at": "Fri Aug 23 13:43:26 +0000 2019",
 		"id": 1164895927833976800,
 		"id_str": "1164895927833976833",
