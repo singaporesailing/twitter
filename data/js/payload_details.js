755aa7186029b8778bfe071b64355d3b2a64b7d5
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-09-09 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1810,
-	"created_at": "Sat Aug 31 06:12:14 +0000 2019",
+	"tweets": 1811,
+	"created_at": "Mon Sep 09 06:12:14 +0000 2019",
 	"lang": "en"
 }
---
+++ data/js/tweets/2019_09.js
@@ -0,0 +1,223 @@
+Grailbird.data.tweets_2019_09 = 
+[
+	{
+		"created_at": "Sun Sep 08 11:18:38 +0000 2019",
+		"id": 1170657694631518200,
+		"id_str": "1170657694631518208",
+		"full_text": "It’s the last weekend before the NSC Cup Series II next week! We hope you’ve been training hard and prepped your boat well, see you soon 😜!⠀\n⠀\n#NSCCup2 #SailforSG #ItTakesAVillage #MakingWavesTogether #raisingchampions https://t.co/VYOheWzRNh https://t.co/Zcbh5V75Q5",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			242
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "NSCCup2",
+					"indices": [
+						143,
+						151
+					]
+				},
+				{
+					"text": "SailforSG",
+					"indices": [
+						152,
+						162
+					]
+				},
+				{
+					"text": "ItTakesAVillage",
+					"indices": [
+						163,
+						179
+					]
+				},
+				{
+					"text": "MakingWavesTogether",
+					"indices": [
+						180,
+						200
+					]
+				},
+				{
+					"text": "raisingchampions",
+					"indices": [
+						201,
+						218
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/VYOheWzRNh",
+					"expanded_url": "https://www.instagram.com/p/B2JePgzgWpR/",
+					"display_url": "instagram.com/p/B2JePgzgWpR/",
+					"indices": [
+						219,
+						242
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1170657693582934000,
+					"id_str": "1170657693582934016",
+					"indices": [
+						243,
+						266
+					],
+					"media_url": "http://pbs.twimg.com/media/ED8C-EuWwAAp3j2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED8C-EuWwAAp3j2.jpg",
+					"url": "https://t.co/Zcbh5V75Q5",
+					"display_url": "pic.twitter.com/Zcbh5V75Q5",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1170657694631518208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1170657693582934000,
+					"id_str": "1170657693582934016",
+					"indices": [
+						243,
+						266
+					],
+					"media_url": "http://pbs.twimg.com/media/ED8C-EuWwAAp3j2.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/ED8C-EuWwAAp3j2.jpg",
+					"url": "https://t.co/Zcbh5V75Q5",
+					"display_url": "pic.twitter.com/Zcbh5V75Q5",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1170657694631518208/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "SingaporeSailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7317,
+			"friends_count": 5221,
+			"listed_count": 44,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6414,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1825,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "It’s the last weekend before the NSC Cup Series II next week! We hope you’ve been training hard and prepped your boat well, see you soon 😜!⠀\n⠀\n#NSCCup2 #SailforSG #ItTakesAVillage #MakingWavesTogether #raisingchampions https://t.co/VYOheWzRNh https://t.co/Zcbh5V75Q5"
+	}
+]
