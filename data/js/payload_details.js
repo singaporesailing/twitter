985489a33664682ca05fc6bebcf83bab74314414
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-09-11 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1811,
-	"created_at": "Mon Sep 09 06:12:14 +0000 2019",
+	"tweets": 1812,
+	"created_at": "Wed Sep 11 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,258 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Tue Sep 10 11:34:14 +0000 2019",
+		"id": 1171386394469687300,
+		"id_str": "1171386394469687296",
+		"full_text": "Our Singapore sailing team windsurfer @mandapanda___ training hard in Lake Garda, Italy! [Repost] Beautiful windy days in Garda 💨🍃😍\n.\n.\n.\n#windsurfing\n#lake\n#garda\n#windy\n#sunny\n#perfect \n#training\n#singaporesailing https://t.co/qz0FQWEJSm https://t.co/1ZkHnQzF1l",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			239
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "windsurfing",
+					"indices": [
+						138,
+						150
+					]
+				},
+				{
+					"text": "lake",
+					"indices": [
+						151,
+						156
+					]
+				},
+				{
+					"text": "garda",
+					"indices": [
+						157,
+						163
+					]
+				},
+				{
+					"text": "windy",
+					"indices": [
+						164,
+						170
+					]
+				},
+				{
+					"text": "sunny",
+					"indices": [
+						171,
+						177
+					]
+				},
+				{
+					"text": "perfect",
+					"indices": [
+						178,
+						186
+					]
+				},
+				{
+					"text": "training",
+					"indices": [
+						188,
+						197
+					]
+				},
+				{
+					"text": "singaporesailing",
+					"indices": [
+						198,
+						215
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "mandapanda___",
+					"name": "Amanda",
+					"id": 3327956304,
+					"id_str": "3327956304",
+					"indices": [
+						38,
+						52
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/qz0FQWEJSm",
+					"expanded_url": "https://www.instagram.com/p/B2OpwHFABJJ/",
+					"display_url": "instagram.com/p/B2OpwHFABJJ/",
+					"indices": [
+						216,
+						239
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1171386393500758000,
+					"id_str": "1171386393500758016",
+					"indices": [
+						240,
+						263
+					],
+					"media_url": "http://pbs.twimg.com/media/EEGZt_dWsAAjave.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EEGZt_dWsAAjave.jpg",
+					"url": "https://t.co/1ZkHnQzF1l",
+					"display_url": "pic.twitter.com/1ZkHnQzF1l",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1171386394469687296/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1171386393500758000,
+					"id_str": "1171386393500758016",
+					"indices": [
+						240,
+						263
+					],
+					"media_url": "http://pbs.twimg.com/media/EEGZt_dWsAAjave.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EEGZt_dWsAAjave.jpg",
+					"url": "https://t.co/1ZkHnQzF1l",
+					"display_url": "pic.twitter.com/1ZkHnQzF1l",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1171386394469687296/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "SingaporeSailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7317,
+			"friends_count": 5226,
+			"listed_count": 44,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6414,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1826,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Our Singapore sailing team windsurfer @mandapanda___ training hard in Lake Garda, Italy! [Repost] Beautiful windy days in Garda 💨🍃😍\n.\n.\n.\n#windsurfing\n#lake\n#garda\n#windy\n#sunny\n#perfect \n#training\n#singaporesailing https://t.co/qz0FQWEJSm https://t.co/1ZkHnQzF1l"
+	},
+	{
 		"created_at": "Sun Sep 08 11:18:38 +0000 2019",
 		"id": 1170657694631518200,
 		"id_str": "1170657694631518208",
