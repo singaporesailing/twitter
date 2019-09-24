09d7825a80ab7717326bf9159a2deb893793ed48
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-09-24 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1813,
-	"created_at": "Fri Sep 20 06:12:15 +0000 2019",
+	"tweets": 1814,
+	"created_at": "Tue Sep 24 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_09.js
+++ data/js/tweets/2019_09.js
@@ -1,6 +1,241 @@
 Grailbird.data.tweets_2019_09 = 
 [
 	{
+		"created_at": "Mon Sep 23 12:50:43 +0000 2019",
+		"id": 1176116683414605800,
+		"id_str": "1176116683414605829",
+		"full_text": "[Repost] @cecilialowruiqi and @kimmy_3129 Escaping the haze in Singapore ⠀\nPortugal here we come 🙌 🛫🇵🇹 ⠀\n⠀\n📸 @luiscarmofraguas ⠀\n⠀\n@sport_singapore | @singaporesailingteam | @gottifredimaffioli | @oakley | @oakleysg | @garminsg | @garmin | @singa… https://t.co/XTqiprWwri https://t.co/L2Zrt0Aby7",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			271
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "oakley",
+					"name": "Oakley",
+					"id": 15069555,
+					"id_str": "15069555",
+					"indices": [
+						196,
+						203
+					]
+				},
+				{
+					"screen_name": "OakleySG",
+					"name": "Oakley Singapore",
+					"id": 554930405,
+					"id_str": "554930405",
+					"indices": [
+						206,
+						215
+					]
+				},
+				{
+					"screen_name": "garminsg",
+					"name": "Garmin Singapore",
+					"id": 3064011488,
+					"id_str": "3064011488",
+					"indices": [
+						218,
+						227
+					]
+				},
+				{
+					"screen_name": "Garmin",
+					"name": "Garmin",
+					"id": 15324722,
+					"id_str": "15324722",
+					"indices": [
+						230,
+						237
+					]
+				},
+				{
+					"screen_name": "singa",
+					"name": "Singa Christophe",
+					"id": 848504849020989400,
+					"id_str": "848504849020989440",
+					"indices": [
+						240,
+						246
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/XTqiprWwri",
+					"expanded_url": "https://www.instagram.com/p/B2wHSmHA1_M/",
+					"display_url": "instagram.com/p/B2wHSmHA1_M/",
+					"indices": [
+						248,
+						271
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1176116682579992600,
+					"id_str": "1176116682579992576",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/EFJn5DeXYAAO8gI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFJn5DeXYAAO8gI.jpg",
+					"url": "https://t.co/L2Zrt0Aby7",
+					"display_url": "pic.twitter.com/L2Zrt0Aby7",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1176116683414605829/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1176116682579992600,
+					"id_str": "1176116682579992576",
+					"indices": [
+						272,
+						295
+					],
+					"media_url": "http://pbs.twimg.com/media/EFJn5DeXYAAO8gI.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EFJn5DeXYAAO8gI.jpg",
+					"url": "https://t.co/L2Zrt0Aby7",
+					"display_url": "pic.twitter.com/L2Zrt0Aby7",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1176116683414605829/photo/1",
+					"type": "photo",
+					"sizes": {
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "SingaporeSailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7296,
+			"friends_count": 5213,
+			"listed_count": 44,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6413,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1828,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 2,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[Repost] @cecilialowruiqi and @kimmy_3129 Escaping the haze in Singapore ⠀\nPortugal here we come 🙌 🛫🇵🇹 ⠀\n⠀\n📸 @luiscarmofraguas ⠀\n⠀\n@sport_singapore | @singaporesailingteam | @gottifredimaffioli | @oakley | @oakleysg | @garminsg | @garmin | @singa… https://t.co/XTqiprWwri https://t.co/L2Zrt0Aby7"
+	},
+	{
 		"created_at": "Thu Sep 19 11:57:32 +0000 2019",
 		"id": 1174653747533885400,
 		"id_str": "1174653747533885441",
