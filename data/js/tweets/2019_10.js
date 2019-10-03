71f4166b9a2139e71862b471ecd70c3bbda55658
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-10-03 14:12:15 +0800

    Added 1 tweets

---
+++ data/js/tweets/2019_10.js
@@ -0,0 +1,216 @@
+Grailbird.data.tweets_2019_10 = 
+[
+	{
+		"created_at": "Wed Oct 02 12:10:08 +0000 2019",
+		"id": 1179367960999317500,
+		"id_str": "1179367960999317510",
+		"full_text": "Just a couple Lasers hanging out at the National Sailing Centre! 😜 Where are you sailing this week? 🤔⛵️⛵️⠀\n⠀\n#SailforSG #ItTakesAVillage #MakingWavesTogether #raisingchampions https://t.co/e1QsuHl7gt https://t.co/jDBvnZKXcw",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			199
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "SailforSG",
+					"indices": [
+						109,
+						119
+					]
+				},
+				{
+					"text": "ItTakesAVillage",
+					"indices": [
+						120,
+						136
+					]
+				},
+				{
+					"text": "MakingWavesTogether",
+					"indices": [
+						137,
+						157
+					]
+				},
+				{
+					"text": "raisingchampions",
+					"indices": [
+						158,
+						175
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/e1QsuHl7gt",
+					"expanded_url": "https://www.instagram.com/p/B3HUf0zgGPP/",
+					"display_url": "instagram.com/p/B3HUf0zgGPP/",
+					"indices": [
+						176,
+						199
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1179367960047169500,
+					"id_str": "1179367960047169536",
+					"indices": [
+						200,
+						223
+					],
+					"media_url": "http://pbs.twimg.com/media/EF306T4WsAAoDAK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EF306T4WsAAoDAK.jpg",
+					"url": "https://t.co/jDBvnZKXcw",
+					"display_url": "pic.twitter.com/jDBvnZKXcw",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1179367960999317510/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1179367960047169500,
+					"id_str": "1179367960047169536",
+					"indices": [
+						200,
+						223
+					],
+					"media_url": "http://pbs.twimg.com/media/EF306T4WsAAoDAK.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EF306T4WsAAoDAK.jpg",
+					"url": "https://t.co/jDBvnZKXcw",
+					"display_url": "pic.twitter.com/jDBvnZKXcw",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1179367960999317510/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"large": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"small": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 426,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "SingaporeSailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7297,
+			"friends_count": 5207,
+			"listed_count": 44,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6411,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1830,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "Just a couple Lasers hanging out at the National Sailing Centre! 😜 Where are you sailing this week? 🤔⛵️⛵️⠀\n⠀\n#SailforSG #ItTakesAVillage #MakingWavesTogether #raisingchampions https://t.co/e1QsuHl7gt https://t.co/jDBvnZKXcw"
+	}
+]
