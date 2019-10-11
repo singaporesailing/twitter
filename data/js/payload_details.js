b10beb50d4f07b66f1f445a440d2094a73aa265d
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-10-11 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1816,
-	"created_at": "Thu Oct 03 06:12:14 +0000 2019",
+	"tweets": 1817,
+	"created_at": "Fri Oct 11 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,6 +1,201 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Thu Oct 10 12:11:36 +0000 2019",
+		"id": 1182267435484946400,
+		"id_str": "1182267435484946432",
+		"full_text": "[Repost] @jd.sailing Just back from a 3 week training camp at the Olympic sailing venue in Enoshima. Really great to be training with the Austrian 🇦🇹 and Japanese 🇯🇵 teams these weeks. Less extreme weather this time but learnt a lot about the shifty east… https://t.co/rBKpQDAIi7 https://t.co/FdXafwYx14",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			279
+		],
+		"entities": {
+			"hashtags": [],
+			"symbols": [],
+			"user_mentions": [
+				{
+					"screen_name": "jd",
+					"name": "jon bradford",
+					"id": 781240,
+					"id_str": "781240",
+					"indices": [
+						9,
+						12
+					]
+				}
+			],
+			"urls": [
+				{
+					"url": "https://t.co/rBKpQDAIi7",
+					"expanded_url": "https://www.instagram.com/p/B3b1yeagceC/",
+					"display_url": "instagram.com/p/B3b1yeagceC/",
+					"indices": [
+						256,
+						279
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1182267434574778400,
+					"id_str": "1182267434574778368",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/EGhB98NXkAApbYe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGhB98NXkAApbYe.jpg",
+					"url": "https://t.co/FdXafwYx14",
+					"display_url": "pic.twitter.com/FdXafwYx14",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1182267435484946432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1182267434574778400,
+					"id_str": "1182267434574778368",
+					"indices": [
+						280,
+						303
+					],
+					"media_url": "http://pbs.twimg.com/media/EGhB98NXkAApbYe.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EGhB98NXkAApbYe.jpg",
+					"url": "https://t.co/FdXafwYx14",
+					"display_url": "pic.twitter.com/FdXafwYx14",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1182267435484946432/photo/1",
+					"type": "photo",
+					"sizes": {
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "SingaporeSailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7289,
+			"friends_count": 5203,
+			"listed_count": 44,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6409,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1831,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 1,
+		"favorite_count": 5,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "[Repost] @jd.sailing Just back from a 3 week training camp at the Olympic sailing venue in Enoshima. Really great to be training with the Austrian 🇦🇹 and Japanese 🇯🇵 teams these weeks. Less extreme weather this time but learnt a lot about the shifty east… https://t.co/rBKpQDAIi7 https://t.co/FdXafwYx14"
+	},
+	{
 		"created_at": "Wed Oct 02 12:10:08 +0000 2019",
 		"id": 1179367960999317500,
 		"id_str": "1179367960999317510",
