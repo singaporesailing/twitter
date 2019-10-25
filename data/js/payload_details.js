aa891ba95785e8193b252049e5aeaf21eac86ace
singaporesailing <39143391+singaporesailing@users.noreply.github.com>
2019-10-25 14:12:15 +0800

    Added 1 tweets

--- data/js/payload_details.js
+++ data/js/payload_details.js
@@ -1,5 +1,5 @@
 var payload_details = {
-	"tweets": 1818,
-	"created_at": "Sun Oct 20 06:12:14 +0000 2019",
+	"tweets": 1819,
+	"created_at": "Fri Oct 25 06:12:14 +0000 2019",
 	"lang": "en"
 }
--- data/js/tweets/2019_10.js
+++ data/js/tweets/2019_10.js
@@ -1,10 +1,209 @@
 Grailbird.data.tweets_2019_10 = 
 [
 	{
+		"created_at": "Thu Oct 24 12:43:15 +0000 2019",
+		"id": 1187348827508007000,
+		"id_str": "1187348827508006912",
+		"full_text": "If you fall behind, run faster, never give up, never surrender and rise up against the odds. #inspire #motivation https://t.co/JoZp1MtpfN https://t.co/9lg3orDHLR",
+		"truncated": false,
+		"display_text_range": [
+			0,
+			137
+		],
+		"entities": {
+			"hashtags": [
+				{
+					"text": "inspire",
+					"indices": [
+						93,
+						101
+					]
+				},
+				{
+					"text": "motivation",
+					"indices": [
+						102,
+						113
+					]
+				}
+			],
+			"symbols": [],
+			"user_mentions": [],
+			"urls": [
+				{
+					"url": "https://t.co/JoZp1MtpfN",
+					"expanded_url": "https://www.instagram.com/p/B3_6ihtAtaW/",
+					"display_url": "instagram.com/p/B3_6ihtAtaW/",
+					"indices": [
+						114,
+						137
+					]
+				}
+			],
+			"media": [
+				{
+					"id": 1187348826300006400,
+					"id_str": "1187348826300006402",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/EHpPd3gWwAI9nBf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EHpPd3gWwAI9nBf.jpg",
+					"url": "https://t.co/9lg3orDHLR",
+					"display_url": "pic.twitter.com/9lg3orDHLR",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1187348827508006912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"extended_entities": {
+			"media": [
+				{
+					"id": 1187348826300006400,
+					"id_str": "1187348826300006402",
+					"indices": [
+						138,
+						161
+					],
+					"media_url": "http://pbs.twimg.com/media/EHpPd3gWwAI9nBf.jpg",
+					"media_url_https": "https://pbs.twimg.com/media/EHpPd3gWwAI9nBf.jpg",
+					"url": "https://t.co/9lg3orDHLR",
+					"display_url": "pic.twitter.com/9lg3orDHLR",
+					"expanded_url": "https://twitter.com/SingaporeSail/status/1187348827508006912/photo/1",
+					"type": "photo",
+					"sizes": {
+						"thumb": {
+							"w": 150,
+							"h": 150,
+							"resize": "crop"
+						},
+						"small": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"large": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						},
+						"medium": {
+							"w": 640,
+							"h": 640,
+							"resize": "fit"
+						}
+					}
+				}
+			]
+		},
+		"source": "<a href=\"https://ifttt.com\" rel=\"nofollow\">IFTTT</a>",
+		"in_reply_to_status_id": null,
+		"in_reply_to_status_id_str": null,
+		"in_reply_to_user_id": null,
+		"in_reply_to_user_id_str": null,
+		"in_reply_to_screen_name": null,
+		"user": {
+			"id": 906430548641439700,
+			"id_str": "906430548641439744",
+			"name": "SingaporeSailing 🇸🇬 ⛵️",
+			"screen_name": "SingaporeSail",
+			"location": "Singapore",
+			"description": "Official Twitter account of Singapore Sailing Federation & Singapore Sailing Team. Join our Olympic voyage to bring S'pore glory from sea #SailforSG",
+			"url": "https://t.co/x1KTJWBNjK",
+			"entities": {
+				"url": {
+					"urls": [
+						{
+							"url": "https://t.co/x1KTJWBNjK",
+							"expanded_url": "http://sailing.org.sg",
+							"display_url": "sailing.org.sg",
+							"indices": [
+								0,
+								23
+							]
+						}
+					]
+				},
+				"description": {
+					"urls": []
+				}
+			},
+			"protected": false,
+			"followers_count": 7272,
+			"friends_count": 5194,
+			"listed_count": 44,
+			"created_at": "Sat Sep 09 08:14:18 +0000 2017",
+			"favourites_count": 6408,
+			"utc_offset": null,
+			"time_zone": null,
+			"geo_enabled": true,
+			"verified": false,
+			"statuses_count": 1833,
+			"lang": null,
+			"contributors_enabled": false,
+			"is_translator": false,
+			"is_translation_enabled": false,
+			"profile_background_color": "000000",
+			"profile_background_image_url": "http://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_image_url_https": "https://abs.twimg.com/images/themes/theme1/bg.png",
+			"profile_background_tile": false,
+			"profile_image_url": "http://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_image_url_https": "https://pbs.twimg.com/profile_images/906431693329883137/sqULnWb6_normal.jpg",
+			"profile_banner_url": "https://pbs.twimg.com/profile_banners/906430548641439744/1505034054",
+			"profile_link_color": "D10000",
+			"profile_sidebar_border_color": "000000",
+			"profile_sidebar_fill_color": "000000",
+			"profile_text_color": "000000",
+			"profile_use_background_image": false,
+			"has_extended_profile": false,
+			"default_profile": false,
+			"default_profile_image": false,
+			"following": false,
+			"follow_request_sent": false,
+			"notifications": false,
+			"translator_type": "none"
+		},
+		"geo": null,
+		"coordinates": null,
+		"place": null,
+		"contributors": null,
+		"is_quote_status": false,
+		"retweet_count": 0,
+		"favorite_count": 4,
+		"favorited": false,
+		"retweeted": false,
+		"possibly_sensitive": false,
+		"lang": "en",
+		"text": "If you fall behind, run faster, never give up, never surrender and rise up against the odds. #inspire #motivation https://t.co/JoZp1MtpfN https://t.co/9lg3orDHLR"
+	},
+	{
 		"created_at": "Sat Oct 19 10:55:21 +0000 2019",
 		"id": 1185509735795171300,
 		"id_str": "1185509735795171329",
-		"full_text": "@sgolympics Can’t surf in Singapore 🏄‍♂️? Try kiteboarding instead! With the wind 💨 powering you at high speeds through the water, you’ll definitely get a kick out of this sport.⁠⠀\n⁠⠀\nAnd if you didn’t know, our very own Maximilian Maeder is now comp… https://t.co/G3vyRqT60c https://t.co/mr4A2T2j8m",
+		"full_text": "@sgolympics Can’t surf in Singapore 🏄♂️? Try kiteboarding instead! With the wind 💨 powering you at high speeds through the water, you’ll definitely get a kick out of this sport.⠀\n⠀\nAnd if you didn’t know, our very own Maximilian Maeder is now comp… https://t.co/G3vyRqT60c https://t.co/mr4A2T2j8m",
 		"truncated": false,
 		"display_text_range": [
 			0,
@@ -193,7 +392,7 @@ Grailbird.data.tweets_2019_10 =
 		"retweeted": false,
 		"possibly_sensitive": false,
 		"lang": "en",
-		"text": "@sgolympics Can’t surf in Singapore 🏄‍♂️? Try kiteboarding instead! With the wind 💨 powering you at high speeds through the water, you’ll definitely get a kick out of this sport.⁠⠀\n⁠⠀\nAnd if you didn’t know, our very own Maximilian Maeder is now comp… https://t.co/G3vyRqT60c https://t.co/mr4A2T2j8m"
+		"text": "@sgolympics Can’t surf in Singapore 🏄♂️? Try kiteboarding instead! With the wind 💨 powering you at high speeds through the water, you’ll definitely get a kick out of this sport.⠀\n⠀\nAnd if you didn’t know, our very own Maximilian Maeder is now comp… https://t.co/G3vyRqT60c https://t.co/mr4A2T2j8m"
 	},
 	{
 		"created_at": "Thu Oct 10 12:11:36 +0000 2019",
